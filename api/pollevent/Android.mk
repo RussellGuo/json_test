bb988f9c73f21706b713e3556d934512d55d2c1e
Guo Qiang <guoqiang@huaqin.com>
2019-02-23 21:56:47 +0800

    try static libraries.
    .
    
    Change-Id: I65012b2543492168af18a55f3bcfa82e7b4028ba

--- api/pollevent/Android.mk
+++ api/pollevent/Android.mk
@@ -22,14 +22,15 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDE_DIRS += $(LOCAL_PATH)/include
 LOCAL_EXPORT_CPP_INCLUDE_DIRS += $(LOCAL_PATH)/include
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := main.c
 
-LOCAL_SHARED_LIBRARIES := libpoll_event_api libtimer_api libipc_cmd
+LOCAL_STATIC_LIBRARIES := libtimer_api
+LOCAL_SHARED_LIBRARIES := libpoll_event_api
 LOCAL_MODULE := poll_event_api_test
 LOCAL_CFLAGS += -std=c99
 
--- api/tts/Android.mk
+++ api/tts/Android.mk
@@ -17,11 +17,12 @@ LOCAL_SRC_FILES := \
 	tts.c tts_api.c pcm_play.c
 
 LOCAL_WHOLE_STATIC_LIBRARIES += libyt_tts
-LOCAL_SHARED_LIBRARIES:= libtinyalsa libipc_cmd libpoll_event_api
+LOCAL_SHARED_LIBRARIES:= libtinyalsa libpoll_event_api 
+LOCAL_STATIC_LIBRARIES:= libipc_cmd libtimer_api
 LOCAL_MODULE := libtts
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
@@ -29,7 +30,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	main.c
 
-LOCAL_SHARED_LIBRARIES := libtts libtinyalsa libipc_cmd libpoll_event_api
+LOCAL_SHARED_LIBRARIES := libtinyalsa libpoll_event_api
+LOCAL_STATIC_LIBRARIES := libtts libipc_cmd libtimer_api
 LOCAL_MODULE := tts_test
 LOCAL_CFLAGS += -std=c99
 
