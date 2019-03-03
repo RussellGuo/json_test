6620a2ffc043a8cc60faa9a11e4fb492b5e590e0
Guo Qiang <guoqiang@huaqin.com>
2019-03-03 16:02:58 +0800

    [QP506][API][TTS]TTS service/API source code split into 2 directories. It's for an alternate TTS provider.
    .
    
    Change-Id: I61c6909e3d9483a3538ad475e9f2021d189b3922

--- api/tts_srv/Android.mk
+++ api/tts_srv/Android.mk
@@ -24,28 +24,3 @@ LOCAL_CFLAGS += -std=c99
 
 include $(BUILD_EXECUTABLE)
 
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := tts_api.c
-
-LOCAL_CFLAGS += -std=c99
-LOCAL_SHARED_LIBRARIES += libipc_cmd libpoll_event_api
-LOCAL_MODULE := libtts
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
-
-include $(BUILD_SHARED_LIBRARY)
-
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := main.c
-
-LOCAL_SHARED_LIBRARIES := libtts libipc_cmd libpoll_event_api libkey_api
-LOCAL_MODULE := tts_test
-LOCAL_CFLAGS += -std=c99
-
-
-include $(BUILD_EXECUTABLE)
-
