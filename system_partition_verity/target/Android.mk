0567bfafec9fa5ec6d19fcafcbd2c351655f7ff0
Guo Qiang <guoqiang@huaqin.com>
2019-05-03 09:08:48 +0800

    [ZQP506][SYS][DM-VERITY]using static link since it is running without system partition be ready. put it into the root/sbin instead of the /system/bin.
    .
    
    Change-Id: Iff695e084237e092157e356cfb7ec7882c7c4f09

--- system_partition_verity/target/Android.mk
+++ system_partition_verity/target/Android.mk
@@ -6,7 +6,15 @@ LOCAL_CFLAGS += -std=c99
 LOCAL_SRC_FILES := ../common/calc_dir_digest_recursively.c gen_sys_part_sign.c target_fs_config.c
 LOCAL_C_INCLUDES += external/openssl/include $(LOCAL_PATH)/../common
 
-LOCAL_SHARED_LIBRARIES := libssl libcrypto
 LOCAL_MODULE := target_gen_sys_part_sign
+LOCAL_STATIC_LIBRARIES := \
+        libc \
+    libcrypto_static
+
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
 
 include $(BUILD_EXECUTABLE)
