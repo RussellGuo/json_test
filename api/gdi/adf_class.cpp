2962da9e80eaa6a9841b166688152787dd57c1e3
Guo Qiang <guoqiang@huaqin.com>
2019-05-11 16:00:08 +0800

    [QP506][API][GDI]use timer to reduce too fast flushing.
    delay a flushing if last flushing is in drawing.
    if more and more flushing be came very quickly, only the last one is activated.
    
    Change-Id: If80449e29d45addb710e97cf00ac1d30112d5915

--- api/gdi/adf_class.cpp
+++ api/gdi/adf_class.cpp
@@ -11,7 +11,6 @@
 #include <sys/mman.h>
 
 #include <adf/adf.h>
-#include <linux/sync.h>
 
 class adf_t {
 public:
@@ -33,25 +32,12 @@ public:
         return true;
     }
 
-    bool flip() {
-        int curr_fence_fd = adf_interface_simple_post(intf_fd, eng_id, intf_data.current_mode.hdisplay,intf_data.current_mode.vdisplay, format, surf_fd, surf_offset, surf_pitch, -1);
-        if (curr_fence_fd >= 0) {
-            int err;
-            if (last_fence_fd >= 0) {
-                int timeout = 3000;
-                err = ioctl(last_fence_fd, SYNC_IOC_WAIT, &timeout);
-                if (err < 0) {
-                    perror("adf sync fence wait");
-                }
-                close(last_fence_fd);
-            } else {
-                err = 0;
-            }
-            last_fence_fd = curr_fence_fd;
-            return err >= 0;
-        } else {
-            return false;
+    bool flip() const {
+        int ret = adf_interface_simple_post(intf_fd, eng_id, intf_data.current_mode.hdisplay,intf_data.current_mode.vdisplay, format, surf_fd, surf_offset, surf_pitch, -1);
+        if (ret >= 0) {
+            close(ret);
         }
+        return ret >= 0;
     }
 
 
@@ -66,8 +52,6 @@ private:
     __u32 surf_offset;
     __u32 surf_pitch;
     void *surf_base;
-
-    int last_fence_fd = -1;
 
     bool init() {
         int err;
@@ -131,8 +115,6 @@ public:
         surf_fd = -1;
         close(intf_fd);
         intf_fd = -1;
-        close(last_fence_fd);
-        last_fence_fd = -1;
 
         adf_free_interface_data(&intf_data);
         fprintf(stderr, "closed\n");
--- api/gdi/gdi.c
+++ api/gdi/gdi.c
@@ -5,6 +5,8 @@
 #include "adf_class.h"
 #include "simple_draw.h"
 
+#include "timer_api.h"
+
 void initDisplay(void)
 {
     void *fb_ptr;
@@ -20,7 +22,62 @@ void deInitDisplay(void)
     deinitAdfDevice();
 }
 
-void displayFlush(void)
+
+// use timer to reduce too fast flushing.
+// delay a flushing if last flushing is in drawing.
+// if more and more flushing be came very quickly, only the last one is activated.
+
+static timer_id_t gui_flip_timer_id = -1;
+
+#define DRAW_FREQ 15
+#define ONE_TO_NANO (1000 * 1000 * 1000)
+#define DRAW_PERIOD_IN_NS (ONE_TO_NANO / DRAW_FREQ + (ONE_TO_NANO % DRAW_FREQ != 0))
+static struct timespec drawing_end_time;
+static void start_drawing(void)
 {
     AdfFlip();
+
+    struct timespec drawing_begin_time;
+    if (clock_gettime(CLOCK_MONOTONIC, &drawing_begin_time) < 0) {
+        perror("clock_gettime");
+        return;
+    }
+
+    // calc the end time
+    drawing_end_time = drawing_begin_time;
+    drawing_end_time.tv_nsec += DRAW_PERIOD_IN_NS;
+    if (drawing_end_time.tv_nsec >= ONE_TO_NANO) {
+        drawing_end_time.tv_nsec -= ONE_TO_NANO;
+        drawing_end_time.tv_sec += 1;
+    }
+}
+
+static void flip_timeout(timer_id_t id , uint64_t arg)
+{
+    start_drawing();
+}
+
+
+void displayFlush(void)
+{
+    if (gui_flip_timer_id < 0) {
+        gui_flip_timer_id = createSimpleTimer(10000, true, flip_timeout);
+    }
+    struct timespec current;
+    if (clock_gettime(CLOCK_MONOTONIC, &current) < 0) {
+        perror("clock_gettime");
+        return;
+    }
+    if (current.tv_sec > drawing_end_time.tv_sec || (current.tv_sec == drawing_end_time.tv_sec && current.tv_nsec > drawing_end_time.tv_nsec)) {
+        //fprintf(stderr, "Drawing at once\n");
+        start_drawing();
+    } else {
+        //fprintf(stderr, "Drawing later\n");
+        // should delay our flipping
+        struct itimerspec itimerspec;
+        itimerspec.it_value = drawing_end_time;
+        itimerspec.it_interval.tv_sec = itimerspec.it_interval.tv_nsec = 0;
+        modifyTimer(gui_flip_timer_id, 1, &itimerspec);
+
+    }
 }
