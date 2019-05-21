99173665f3f2d2c38b465856e32bb4a4127fdacf
101007192 <baochunyang@huaqin.com>
2019-05-21 18:04:52 +0800

    添加Usbonline接口
    Change-Id: I205ce27d9ec89cf67a69240afd5a3f1893dcf3e8

--- api/battery/main.c
+++ api/battery/main.c
@@ -10,16 +10,18 @@
 
 int main()
 {
-	int Batvol,Temperature,Present;
+	int Batvol,Temperature,Present,Usbonline;
 
 	Batvol=getBatvol();
 	Temperature=getTemperature();
 	Present=getPresent();
+	Usbonline=getUsbonline();
 	getStatus(bat_status.getStatus);
 
 	printf("bat_status.getBatvol=%d\n", Batvol);
   	printf("bat_status.getTemperature=%d\n", Temperature);
  	printf("bat_status.getPresent=%d\n", Present);
+ 	printf("bat_status.getUsbonline=%d\n", Usbonline);
  	printf("bat_status.getStatus=%s\n",bat_status.getStatus);
 	return 0;
 }
