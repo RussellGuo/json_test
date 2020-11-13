b2e0c96a4d51cffc19525a5617d635ae78f239d3
Guo Qiang <guoqiang@huaqin.com>
2020-11-13 12:09:12 +0800

    create a new Keil project. Not ready yet.
    start a project with the GD32E10x chip;
    added the packages of the RTOS and the GD devices support;
    rename the target;
    added a source group;
    added a main function which has nothing to do;
    saved them.
    
    Change-Id: I88e9d2340173dc9f0c1a04b6c3e416f68e175808

---
+++ start-board/RTE/_start_board/RTE_Components.h
@@ -0,0 +1,43 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'start_board' 
+ * Target:  'start_board' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "gd32e10x.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.2 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* GigaDevice::Device:GD32E10x_StdPeripherals:EXTI:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_EXTI
+/* GigaDevice::Device:GD32E10x_StdPeripherals:FMC:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_FMC
+/* GigaDevice::Device:GD32E10x_StdPeripherals:GPIO:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_GPIO
+/* GigaDevice::Device:GD32E10x_StdPeripherals:I2C:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_I2C
+/* GigaDevice::Device:GD32E10x_StdPeripherals:MISC:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_MISC
+/* GigaDevice::Device:GD32E10x_StdPeripherals:PMU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_PMU
+/* GigaDevice::Device:GD32E10x_StdPeripherals:RCU:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_RCU
+/* GigaDevice::Device:GD32E10x_StdPeripherals:TIMER:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_TIMER
+/* GigaDevice::Device:GD32E10x_StdPeripherals:USART:1.0.3 */
+#define RTE_DEVICE_STDPERIPHERALS_USART
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ start-board/main.c
@@ -0,0 +1,7 @@
+int main(void)
+{
+    int count = 0;
+    while (1) {
+        count++;
+    }
+}
