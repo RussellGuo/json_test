cab4d192ff8460d2f5cb2b87f629b7d4e86864c1
Guo Qiang <guoqiang@huaqin.com>
2020-11-30 17:47:00 +0800

    Added the function of data-driven CRC.
    Data-driven methods cannot be adopted on the hardware (to be precise, different computing contexts cannot be maintained, which is necessary in a multi-thread environment), so the software implementation version is updated and the hardware implementation version is deleted.
    
    Change-Id: I1594044abd835767d89dcf69fbfa894b02ed673c

--- host-mcu-common/include/mcu-crc32-soft.h
+++ host-mcu-common/include/mcu-crc32-soft.h
@@ -12,31 +12,41 @@ extern "C" {
 #include <stdbool.h>
 #include <stdlib.h>
 
+
+static inline uint32_t mcu_crc32_soft_init(void)
+{
+    return 0xFFFFFFFFU;
+}
+
+static inline uint32_t mcu_crc32_soft_append(uint32_t last_crc, uint32_t data)
+{
+    uint32_t crc = last_crc;
+    uint32_t v= data;
+    for (int j = 0; j < 4; j++) {
+        crc ^= v & 0xFF000000U;
+        v <<= 8;
+
+        for (int k = 0; k < 8; k++) {
+            uint32_t msb = crc >> 31;
+            crc <<= 1;
+            crc ^= (0 - msb) & 0x04C11DB7;
+        } // end of for k
+   } // end of for j
+
+   return crc;
+}
+
 static inline uint32_t mcu_crc32_soft(const uint32_t *message, size_t len)
 {
-    uint32_t crc = 0xFFFFFFFFU;
+    uint32_t crc = mcu_crc32_soft_init();
 
     for(size_t i = 0; i < len; i++) {
-
         uint32_t v = message[i];
-
-        for (int j = 0; j < 4; j++) {
-            crc ^= v & 0xFF000000U;
-            v <<= 8;
-
-            for (int k = 0; k < 8; k++) {
-                uint32_t msb = crc >> 31;
-                crc <<= 1;
-                crc ^= (0 - msb) & 0x04C11DB7;
-            } // end of for k
-
-        } // end of for j
-
-    } // end of for i
-
+        crc = mcu_crc32_soft_append(crc, v);
+    }
     return crc;
-
 }
+
 #ifdef __cplusplus
 }
 #endif
