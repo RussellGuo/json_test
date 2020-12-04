1d2faa4e8b8c3fa0b18958dd301ad0585fd4248f
Guo Qiang <guoqiang@huaqin.com>
2020-12-04 21:12:56 +0800

    Semantic layer API be added in. [1/2]
    .
    
    Change-Id: I5e69f4883e0d070d0fb784c36869a31d81e758a8

--- host-mcu-common/include/semantic_api.h
+++ host-mcu-common/include/semantic_api.h
@@ -24,11 +24,14 @@ extern "C" {
 
 // For a request, possible return error code
 typedef enum {
-    NO_ERROR        = 0,
-    ERR_CRC         = 1,   // crc check error
-    ERR_MSG_ID      = 2,   // the request code not found
-    ERR_ITEM_COUNT  = 3,   // wrong number of parameters or return list 
-    ERR_EXEC        = 4,   // The message format is correct, but the execution is wrong after the dispatching
+    NO_ERROR        =  0,
+    ERR_CRC         =  1,   // crc check error
+    ERR_MSG_ID      =  2,   // the request code not found
+    ERR_ITEM_COUNT  =  3,   // wrong number of parameters or return list 
+    ERR_NO_IMPL     =  4,   // The message format is correct, but the implementation is not ready
+    ERR_MODE        =  5,   // mode error at the server execution
+    ERR_PARAM       =  6,   // parameter error at the server execution
+    ERR_UNKNOWN     = 15,   // unknown error at the server execution
 } res_error_code_t;
 
 // the msg id, request id mostly
@@ -41,12 +44,71 @@ typedef enum {
     START_FACTORY_TEST     = 13,
 } msg_id_t;
 
-// MCU is the remote calling SERVER
+// MCU is the SERVER
 #if defined(IS_MCU_SIDE)
 #define PROTOCOL_SERVER_SIDE 1
 #else
 #define PROTOCOL_SERVER_SIDE 0
 #endif
+
+// Semantic Layer API
+// The complete action of a semantic layer is started from the host side,
+// reaches the execution part of the MCU, and finally returns to the host side.
+// This description deliberately hides the interactive process of the datagram
+// layer, presenting a simple and necessary interface to the user.
+
+// This series of functions, 'ReqHwFwVersion', is the interface to "started from the host side".
+// The 'ReplyToReqHwFwVersion' series of functions are the interface to the "execution part of the MCU".
+// After "return to host side", the 'DispatchReplyOfReqHwFwVersion' series functions on the host will be invoked.
+
+// The 'ReqHwFwVersion' series of functions are implemented in this semantic layer, and users only need to invoke them.
+// the 'ReplyToReqHwFwVersion' series of functions need to be finalized in various related modules on the MCU side.
+// This semantic layer provides an implementation of the 'WEAK' version, which is to test the protocol implementation
+// before the final implementation. After the final version is completed, the "WEAK" version does not need to be deleted,
+// and the linker will automatically select the version other than "WEAK".
+// the 'DispatchReplyOfReqHwFwVersion' series of functions that need to be finalized in various related modules on the Host side.
+// This semantic layer provides an implementation of the 'WEAK' version. The situation of the 'WEAK' version is the same as
+// the situation in the previous paragraph.
+
+#if PROTOCOL_SERVER_SIDE == 0
+
+// start an action from the host side. Return true means success, otherwise failure
+// The seq is an ID, and the same seq will be returned in the DispatchReplyOfXXX of the same action.
+// Their parameters are consistent with the documentation. Please refer to the documentation
+// "SaIP communication protocol between the host and the MCU"
+bool ReqHwFwVersion(serial_datagram_item_t seq);
+bool ReqRunInfo(serial_datagram_item_t seq);
+bool SetLedConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq);
+bool SetLaserConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq);
+bool SetFlashLightConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq);
+bool StartFactoryTest(serial_datagram_item_t seq);
+
+// A server accessing through a long call chain, and finally reaches the following functions.
+// Every parameter is [in]. the values other than seq comes from 'ReplyToXXX'
+void DispatchReplyOfReqHwFwVersion(const res_error_code_t error_code, const uint32_t HwVersion, const uint32_t FwVersion, serial_datagram_item_t seq);
+void DispatchReplyOfRunInfo(const res_error_code_t error_code, const uint32_t *run_info_list, serial_datagram_item_t seq);
+void DispatchReplyOfSetLedConfig(const res_error_code_t error_code, serial_datagram_item_t seq);
+void DispatchReplyOfSetLaserConfig(const res_error_code_t error_code, serial_datagram_item_t seq);
+void DispatchReplyOfSetFlashLightConfig(const res_error_code_t error_code, serial_datagram_item_t seq);
+void DispatchReplyOfStartFactoryTest(const res_error_code_t error_code, const uint32_t *test_item_list, serial_datagram_item_t seq);
+
+#else
+
+// The interface to the "execution part of the MCU". The ReplyToReqHwFwVersion series of functions are the
+// actual MCU execution functions: parameters are passed in, and the return value and error code are obtained.
+// [out]error_code: the pointer of the error code.
+// [out]every pointer without const qualifier is [out]
+// most parameters are passed from the 'ReqHwFwVersion' likes function,
+//  and [out]values will be returned to 'DispatchReplyOfXXX'
+void ReplyToReqHwFwVersion(res_error_code_t *error_code, uint32_t *HwVersion, uint32_t *FwVersion, serial_datagram_item_t seq);
+void ReplyToRunInfo(res_error_code_t *error_code, uint32_t *run_info_list, serial_datagram_item_t seq);
+void ReplyToSetLedConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, res_error_code_t *error_code, serial_datagram_item_t seq);
+void ReplyToSetLaserConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, res_error_code_t *error_code, serial_datagram_item_t seq);
+void ReplyToSetFlashLightConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, res_error_code_t *error_code, serial_datagram_item_t seq);
+void ReplyToStartFactoryTest(res_error_code_t *error_code, uint32_t *test_item_list, serial_datagram_item_t seq);
+
+#endif
+
 
 #ifdef __cplusplus
 }
--- host-mcu-common/source/semantic_api.c
+++ host-mcu-common/source/semantic_api.c
@@ -9,37 +9,55 @@
  */
 
 #include "semantic_api.h"
+#include <string.h>
+
+// Regardless of the host side or the MCU side, the data delivered by the opposite end is 
+// accessed through the datagram layer. The data at this point is of general uint32_t type.
+// That general data is suitable for processing by looking up the table.
+// At the end of the processing, a specific, strongly typed function(which is the 'ReplyToXXX' 
+//  at the MCU side, or 'DispatchReplyOfXXX' at the host side)is invoking through a short function.
+// The following tables are for MCU side and Host side both. Table looking up and processing are
+// slightly different for the 2 sides.
 
 #if PROTOCOL_SERVER_SIDE
-#define MSG_PROC_PROTOTYPE const serial_datagram_item_t input_item_list[], serial_datagram_item_t output_item_list[], const serial_datagram_item_t seq
+
+// generic processing function protocol definition for the MCU side
+#define MSG_PROC_PROTOTYPE \
+    const serial_datagram_item_t input_item_list[], serial_datagram_item_t output_item_list[], const serial_datagram_item_t seq
 #define MSG_PROC_RET_TYPE res_error_code_t
+
 #else
-#define MSG_PROC_PROTOTYPE const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq
+
+// generic processing function definition for the host side
+#define MSG_PROC_PROTOTYPE \
+   const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq
 #define MSG_PROC_RET_TYPE void
 #endif
 
-MSG_PROC_RET_TYPE              req_hw_fw_msg_proc(MSG_PROC_PROTOTYPE);
-MSG_PROC_RET_TYPE           req_run_info_msg_proc(MSG_PROC_PROTOTYPE);
-MSG_PROC_RET_TYPE         set_led_config_msg_proc(MSG_PROC_PROTOTYPE);
-MSG_PROC_RET_TYPE       set_laser_config_msg_proc(MSG_PROC_PROTOTYPE);
-MSG_PROC_RET_TYPE set_flash_light_config_msg_proc(MSG_PROC_PROTOTYPE);
-MSG_PROC_RET_TYPE     start_factory_test_msg_proc(MSG_PROC_PROTOTYPE);
+// generic processing functions
+static MSG_PROC_RET_TYPE      req_hw_fw_version_msg_proc(MSG_PROC_PROTOTYPE);
+static MSG_PROC_RET_TYPE           req_run_info_msg_proc(MSG_PROC_PROTOTYPE);
+static MSG_PROC_RET_TYPE         set_led_config_msg_proc(MSG_PROC_PROTOTYPE);
+static MSG_PROC_RET_TYPE       set_laser_config_msg_proc(MSG_PROC_PROTOTYPE);
+static MSG_PROC_RET_TYPE set_flash_light_config_msg_proc(MSG_PROC_PROTOTYPE);
+static MSG_PROC_RET_TYPE     start_factory_test_msg_proc(MSG_PROC_PROTOTYPE);
 
 
 typedef MSG_PROC_RET_TYPE (*semantic_msg_process_t)(MSG_PROC_PROTOTYPE);
 
 typedef struct {
-    msg_id_t msg_id;
-    bool direction_to_mcu;
-    uint8_t req_len;
-    uint8_t res_len;
-    semantic_msg_process_t msg_proc;
+    msg_id_t msg_id;                    // as its name
+    bool     direction_to_mcu;          // now it's always true
+    uint8_t  req_len;                   // length of parameters
+    uint8_t  res_len;                   // length of return values
+    semantic_msg_process_t msg_proc;    // generic procssing func
 } sematic_layer_info_t;
 
 static const sematic_layer_info_t *get_sematic_layer_info(serial_datagram_item_t msg_id);
 
+// Here is the table
 static const sematic_layer_info_t sematic_layer_info_tab[] = {
-    { REQ_HW_FW_VERSION     , true , 0,  2, req_hw_fw_msg_proc              },
+    { REQ_HW_FW_VERSION     , true , 0,  2, req_hw_fw_version_msg_proc      },
     { REQ_RUN_INFO          , true , 0,  4, req_run_info_msg_proc           },
     { SET_LED_CONFIG        , true , 2,  1, set_led_config_msg_proc         },
     { SET_LASER_CONFIG      , true , 2,  1, set_laser_config_msg_proc       },
@@ -47,6 +65,9 @@ static const sematic_layer_info_t sematic_layer_info_tab[] = {
     { START_FACTORY_TEST    , true,  0, 10, start_factory_test_msg_proc     },
 };
 
+// find the info by msg_id
+// [in]msg_id, the id
+// return a pointer to the table if found, or else NULL
 static const sematic_layer_info_t *get_sematic_layer_info(serial_datagram_item_t msg_id)
 {
     for (size_t i = 0; i < sizeof sematic_layer_info_tab / sizeof (sematic_layer_info_tab[0]); i++) {
@@ -59,6 +80,8 @@ static const sematic_layer_info_t *get_sematic_layer_info(serial_datagram_item_t
 
 #if PROTOCOL_SERVER_SIDE
 
+// MCU side datagram processing
+
 // Once a datagram be received from remote, this function will be invoked
 // Parameters:
 //   [in]seq, msg_id, data list and data count
@@ -66,26 +89,34 @@ void serial_datagram_arrived(const serial_datagram_item_t seq, const serial_data
     const serial_datagram_item_t *restrict data_list, const size_t len)
 {
     res_error_code_t error_code = NO_ERROR;
+
+    // lookup the table
     const sematic_layer_info_t *const sematic_layer_info = get_sematic_layer_info(msg_id);
+
+    // Check the msg id
     if (sematic_layer_info == 0 || !sematic_layer_info->direction_to_mcu) {
         error_code = ERR_MSG_ID;
     }
+
+    // check parameter length
     if (error_code == NO_ERROR) {
         if (sematic_layer_info->req_len != len) {
             error_code = ERR_ITEM_COUNT;
         }
     }
 
+    // invoke the generic processing function
     serial_datagram_item_t res_items[MAX_DATAGRAM_ITEM_COUNT] = {0};
     if (error_code == NO_ERROR) {
         error_code = sematic_layer_info->msg_proc(data_list, res_items + 1, seq);
     }
 
-    // TODO: now, reply it
+    // reply the response
     size_t res_item_len = sematic_layer_info->res_len;
     res_items[0] = error_code;
     serial_datagram_send(seq, msg_id, res_items, res_item_len + 1);
 }
+
 #else
 
 // Once a datagram be received from remote, this function will be invoked
@@ -94,16 +125,20 @@ void serial_datagram_arrived(const serial_datagram_item_t seq, const serial_data
 void serial_datagram_arrived(const serial_datagram_item_t seq, const serial_datagram_item_t msg_id,
     const serial_datagram_item_t *restrict data_list, const size_t len)
 {
+    // lookup the table
     const sematic_layer_info_t *const sematic_layer_info = get_sematic_layer_info(msg_id);
     if (sematic_layer_info == 0) {
         // log an ERR_MSG_ID
         return;
     }
+
+    // // check return value length
     if (sematic_layer_info->res_len + 1 != len) {
         // log an item count error
         return;
     }
 
+    // invoke the generic processing function
     sematic_layer_info->msg_proc(data_list + 1, (res_error_code_t)data_list[0], seq);
 }
 
@@ -111,37 +146,284 @@ void serial_datagram_arrived(const serial_datagram_item_t seq, const serial_data
 
 #if PROTOCOL_SERVER_SIDE
 
-static void weak_msg_proc(const char *name, const serial_datagram_item_t input_item_list[], serial_datagram_item_t output_item_list[], const serial_datagram_item_t seq)
+
+// a stub of function 'ReplyToReqHwFwVersion'
+__attribute__((weak)) void ReplyToReqHwFwVersion(
+    res_error_code_t *error_code,
+    uint32_t *HwVersion, uint32_t *FwVersion,
+    serial_datagram_item_t seq)
 {
-    (void) name;
-    (void) input_item_list; (void)output_item_list; (void)seq;
+    *HwVersion = 0x55AA; *FwVersion = 0xAA55; // test value
+    *error_code = ERR_NO_IMPL;
+    (void)seq;
 }
 
-#define DEFINE_A_WEAK_MSG_PROC(name) __attribute__((weak)) MSG_PROC_RET_TYPE name(MSG_PROC_PROTOTYPE) \
-{ \
-    weak_msg_proc(#name, input_item_list,output_item_list,seq);\
-    return ERR_EXEC; \
-}
-#else
-#include <stdio.h>
-static void weak_msg_proc(const char *name, const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq)
+// a stub of function 'ReplyToRunInfo'
+__attribute__((weak)) void ReplyToRunInfo(
+    res_error_code_t *error_code,
+    uint32_t *run_info_list, serial_datagram_item_t seq)
 {
-    fprintf(stderr, "got a reponse of '%s' #%X, return %x:", name, seq, error_code);
-    for(size_t i = 0; i < MAX_DATAGRAM_ITEM_COUNT; i++) {
-        fprintf(stderr, " %X", input_item_list[i]);
+    run_info_list[0] = 0xFF; run_info_list[1] = 0xEE; run_info_list[2] = 0xDD; run_info_list[3] = 0xCC; // test value
+    *error_code = ERR_NO_IMPL;
+    (void)seq;
+}
+
+// a stub of function 'ReplyToSetLedConfig'
+__attribute__((weak)) void ReplyToSetLedConfig(
+    serial_datagram_item_t mode,
+    serial_datagram_item_t mode_param,
+    res_error_code_t *error_code,  serial_datagram_item_t seq)
+{
+    *error_code = ERR_NO_IMPL;
+    (void) mode; (void) mode_param; (void)seq;
+}
+
+// a stub of function 'ReplyToSetLaserConfig'
+__attribute__((weak)) void ReplyToSetLaserConfig(
+    serial_datagram_item_t mode,
+    serial_datagram_item_t mode_param,
+    res_error_code_t *error_code, serial_datagram_item_t seq)
+{
+    *error_code = ERR_NO_IMPL;
+    (void) mode; (void) mode_param; (void)seq;
+}
+
+// a stub of function 'ReplyToSetFlashLightConfig'
+__attribute__((weak)) void ReplyToSetFlashLightConfig(
+    serial_datagram_item_t mode,
+    serial_datagram_item_t mode_param,
+    res_error_code_t *error_code, serial_datagram_item_t seq)
+{
+    *error_code = ERR_NO_IMPL;
+    (void) mode; (void) mode_param; (void)seq;
+}
+
+// a stub of function 'ReplyToStartFactoryTest'
+__attribute__((weak)) void ReplyToStartFactoryTest(
+    res_error_code_t *error_code,
+    uint32_t *test_item_list,
+    serial_datagram_item_t seq)
+{
+    memset(test_item_list, 0xBB, 10 * sizeof(test_item_list[0]));
+    *error_code = ERR_NO_IMPL;
+    (void)seq;
+}
+
+
+// generic processing function to specific function for message REQ_HW_FW_VERSION
+static res_error_code_t req_hw_fw_version_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)input_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToReqHwFwVersion(&error_code, output_item_list + 0, output_item_list + 1, seq);
+    return error_code;
+}
+
+// generic processing function to specific function for message REQ_RUN_INFO
+static res_error_code_t req_run_info_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)input_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToRunInfo(&error_code, output_item_list, seq);
+    return error_code;
+}
+
+// generic processing function to specific function for message SET_LED_CONFIG
+static res_error_code_t set_led_config_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)output_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToSetLedConfig(input_item_list[0], input_item_list[1], &error_code, seq);
+    return error_code;
+}
+
+// generic processing function to specific function for message SET_LASER_CONFIG
+static res_error_code_t set_laser_config_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)output_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToSetLedConfig(input_item_list[0], input_item_list[1], &error_code, seq);
+    return error_code;
+}
+
+// generic processing function to specific function for message SET_FLASH_LIGHT_CONFIG
+static res_error_code_t set_flash_light_config_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)output_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToSetLedConfig(input_item_list[0], input_item_list[1], &error_code, seq);
+    return error_code;
+}
+
+// generic processing function to specific function for message START_FACTORY_TEST
+static res_error_code_t start_factory_test_msg_proc(
+    const serial_datagram_item_t input_item_list[],
+    serial_datagram_item_t output_item_list[],
+    const serial_datagram_item_t seq)
+{
+    (void)input_item_list;
+    res_error_code_t error_code = ERR_UNKNOWN;
+    ReplyToStartFactoryTest(&error_code, output_item_list, seq);
+    return error_code;
+}
+
+#endif
+
+
+#if !PROTOCOL_SERVER_SIDE
+#include <stdio.h>
+
+// host side, send request to the MCU
+// These functions are specific functions, converted into general data transmission
+bool ReqHwFwVersion(serial_datagram_item_t seq)
+{
+    bool ret = serial_datagram_send(seq, REQ_HW_FW_VERSION, NULL, 0);
+    return ret;
+}
+
+bool ReqRunInfo(serial_datagram_item_t seq)
+{
+    bool ret = serial_datagram_send(seq, REQ_RUN_INFO, NULL, 0);
+    return ret;
+}
+
+bool SetLedConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq)
+{
+    const serial_datagram_item_t param_list[] = { mode, mode_param };
+    bool ret = serial_datagram_send(seq, SET_LED_CONFIG, param_list, sizeof param_list / sizeof(param_list[0]));
+    return ret;
+}
+
+bool SetLaserConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq)
+{
+    const serial_datagram_item_t param_list[] = { mode, mode_param };
+    bool ret = serial_datagram_send(seq, SET_LASER_CONFIG, param_list, sizeof param_list / sizeof(param_list[0]));
+    return ret;
+}
+
+bool SetFlashLightConfig(serial_datagram_item_t mode, serial_datagram_item_t mode_param, serial_datagram_item_t seq)
+{
+    const serial_datagram_item_t param_list[] = { mode, mode_param };
+    bool ret = serial_datagram_send(seq, SET_FLASH_LIGHT_CONFIG, param_list, sizeof param_list / sizeof(param_list[0]));
+    return ret;
+}
+
+bool StartFactoryTest(serial_datagram_item_t seq)
+{
+    bool ret = serial_datagram_send(seq, START_FACTORY_TEST, NULL, 0);
+    return ret;
+}
+
+// generic processing function to specific function for message REQ_HW_FW_VERSION
+static void req_hw_fw_version_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    DispatchReplyOfReqHwFwVersion(error_code, input_item_list[0], input_item_list[1], seq);
+}
+
+// generic processing function to specific function for message REQ_RUN_INFO
+static void req_run_info_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    DispatchReplyOfRunInfo(error_code, input_item_list, seq);
+}
+
+// generic processing function to specific function for message SET_LED_CONFIG
+static void set_led_config_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    (void)input_item_list;
+    DispatchReplyOfSetLedConfig(error_code, seq);
+}
+
+// generic processing function to specific function for message SET_LASER_CONFIG
+static void set_laser_config_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    (void)input_item_list;
+    DispatchReplyOfSetLaserConfig(error_code, seq);
+}
+
+// generic processing function to specific function for message SET_FLASH_LIGHT_CONFIG
+static void set_flash_light_config_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    (void)input_item_list;
+    DispatchReplyOfSetFlashLightConfig(error_code, seq);
+}
+
+// generic processing function to specific function for message START_FACTORY_TEST
+static void start_factory_test_msg_proc(
+    const serial_datagram_item_t input_item_list[], res_error_code_t error_code, const serial_datagram_item_t seq) 
+{
+    DispatchReplyOfStartFactoryTest(error_code, input_item_list, seq);
+}
+
+// a stub of function 'DispatchReplyOfReqHwFwVersion'
+__attribute__((weak)) void DispatchReplyOfReqHwFwVersion(
+    const res_error_code_t error_code, const uint32_t HwVersion, const uint32_t FwVersion, serial_datagram_item_t seq)
+{
+    fprintf(stderr, "received MCU hw/fw version %04X %04X, error_code %u, seq %u\n", HwVersion, FwVersion, error_code, seq);
+}
+
+// a stub of function 'DispatchReplyOfRunInfo'
+__attribute__((weak)) void DispatchReplyOfRunInfo(
+    const res_error_code_t error_code, const uint32_t *run_info_list, serial_datagram_item_t seq)
+{
+    uint32_t total_datagram          = run_info_list[0];
+    uint32_t bad_format_datagram     = run_info_list[1];
+    uint32_t total_skipped_uart_recv = run_info_list[2];
+    uint32_t total_inner_error       = run_info_list[3];
+    fprintf(stderr,
+        "received MCU run info total_datagram, bad_format_datagram, total_skipped_uart_recv, total_inner_error,"
+        " %X %X %X %X, error_code %u, seq %u\n",
+        total_datagram, bad_format_datagram, total_skipped_uart_recv, total_inner_error,
+        error_code, seq);
+}
+
+// a stub of function 'DispatchReplyOfSetLedConfig'
+__attribute__((weak)) void DispatchReplyOfSetLedConfig(const res_error_code_t error_code, serial_datagram_item_t seq)
+{
+    fprintf(stderr, "received MCU set led config error_code %u, seq %u\n", error_code, seq);
+}
+
+// a stub of function 'DispatchReplyOfSetLaserConfig'
+__attribute__((weak)) void DispatchReplyOfSetLaserConfig(const res_error_code_t error_code, serial_datagram_item_t seq)
+{
+    fprintf(stderr, "received MCU set laser config error_code %u, seq %u\n", error_code, seq);
+}
+
+// a stub of function 'DispatchReplyOfSetFlashLightConfig'
+__attribute__((weak)) void DispatchReplyOfSetFlashLightConfig(const res_error_code_t error_code, serial_datagram_item_t seq)
+{
+    fprintf(stderr, "received MCU set flash light config error_code %u, seq %u\n", error_code, seq);
+}
+
+// a stub of function 'DispatchReplyOfStartFactoryTest'
+__attribute__((weak)) void DispatchReplyOfStartFactoryTest(
+    const res_error_code_t error_code, const uint32_t *test_item_list, serial_datagram_item_t seq)
+{
+    fprintf(stderr, "received MCU factory test result error_code %u, seq %u,", error_code, seq);
+    for (int i = 0; i < 10; i++) {
+        fprintf(stderr, " %X", test_item_list[i]);
     }
     fprintf(stderr, "\n");
 }
 
-#define DEFINE_A_WEAK_MSG_PROC(name) __attribute__((weak)) MSG_PROC_RET_TYPE name(MSG_PROC_PROTOTYPE) \
-{ \
-    weak_msg_proc(#name, input_item_list, error_code, seq); \
-}
-#endif
 
-DEFINE_A_WEAK_MSG_PROC(req_hw_fw_msg_proc)
-DEFINE_A_WEAK_MSG_PROC(req_run_info_msg_proc)
-DEFINE_A_WEAK_MSG_PROC(set_led_config_msg_proc)
-DEFINE_A_WEAK_MSG_PROC(set_laser_config_msg_proc)
-DEFINE_A_WEAK_MSG_PROC(set_flash_light_config_msg_proc)
-DEFINE_A_WEAK_MSG_PROC(start_factory_test_msg_proc)
+#endif
