89f1e59f8ab3cefaa48727781f7085ecf57e5e5f
Guo Qiang <guoqiang@huaqin.com>
2020-12-08 11:39:55 +0800

    create SAIP project step 1
    copy from start-board project
    
    Change-Id: I7499765368547966ef0269c4bd29fcdb02db138e

---
+++ saip-board/main.c
@@ -0,0 +1,27 @@
+#include "gd32e10x.h"
+#include "cmsis_os2.h"
+
+#include "uart_io_api.h"
+
+#include "task_serial_datagram_recv.h"
+
+#include "service_led.h"
+#include "service_laser.h"
+#include "service_flashlight.h"
+
+__NO_RETURN int main(void)
+{
+    osKernelInitialize();
+    NVIC_SetPriorityGrouping (3);                    // setup priority grouping
+
+    init_uart_io_api();
+
+    init_thread_of_serial_datagram_recv();
+
+    init_thread_of_service_led();
+    init_thread_of_service_laser();
+    init_thread_of_service_flashlight();
+
+    osKernelStart();                                 // start RTX kernel
+    while(1);
+}
---
+++ saip-board/service_flashlight_hw.c
@@ -0,0 +1,73 @@
+/*
+ * service_flashlight_hw.c
+ *
+ *
+ * This is a stub module, which is used to develop flashlight services on the start board. The code is
+ * copied from the LED module and slightly modified for use in the R&D process only.
+ *
+ * hardware-depended part of flashlight service for the start-board,
+ *  only used in the development process.
+ *
+ *  Created on: Dec 7, 2020
+ *      Author: Guo Qiang
+ */
+
+#include "service_flashlight.h"
+
+#include "cmsis_os2.h"
+#include "gd32e10x.h"
+
+#include "semantic_api.h"
+
+// const of this module
+#define LED_GPIO_PORT  GPIOA
+#define LED_GPIO_PIN   GPIO_PIN_8
+
+
+// The hardware initialization function required for the operation of the flashlight module.
+void flashlight_hw_init(void)
+{
+    /* configure LED GPIO port */
+    rcu_periph_clock_enable(RCU_GPIOA);
+    gpio_init(LED_GPIO_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,LED_GPIO_PIN);
+}
+
+
+// Perform the service according to the specified mode and mode parameter,
+// and return immediately if a new request is received during the period,
+// otherwise it will loop indefinitely
+// parameters:
+// [in] flashlight_mode, flashlight_mode_param as the protocol
+// [in] new_request_flag: when the underlying protocol modifies the flashlight mode and/or mode parameter,
+//    the flag of this thread will be set. This is an notice to change mode.
+// Note: On the start board, the factory test of the LED is simulated with low flashing LED.
+#define START_BOARD_LED_FLASH_HALF_PERIOD 256
+void flashlight_running_until_new_request_arrived(uint32_t flashlight_mode, uint32_t flashlight_mode_param, uint32_t new_request_flag)
+{
+    (void)flashlight_mode_param;
+
+    if (flashlight_mode == MCU_MODULE_OFF) {
+        // OFF mode, turn off the LED
+        gpio_bit_write(LED_GPIO_PORT, LED_GPIO_PIN, RESET);
+
+        //  and wait the configuration changed flag
+        int32_t wait = osThreadFlagsWait(new_request_flag, osFlagsWaitAny, osWaitForever);
+        if (wait == new_request_flag)
+            return;
+    }
+
+    if (flashlight_mode == FLASHLIGHT_MODE_FACTORY_TEST) {
+        // factory test
+        for(bool value = 0;; value ^= 1) {
+            // toggle the LED
+            gpio_bit_write(LED_GPIO_PORT, LED_GPIO_PIN, value ? SET : RESET);
+
+            // wait the flag or the timer out
+            uint32_t wait = osThreadFlagsWait(new_request_flag, osFlagsWaitAny, START_BOARD_LED_FLASH_HALF_PERIOD);
+            if (wait == new_request_flag) {
+                return;
+            }
+            // timer out, push to the next phase of the flashlight
+        }
+    }
+}
