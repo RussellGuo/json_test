386acb434446b619db134a4805fd635244a7789e
101002537 <guoqiang@huaqin.com>
2021-11-18 18:20:28 +0800

    program project file and base file structure created.
    .

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include "json.hpp"
+
+int main()
+{
+    auto j2 = R"(
+{
+	"version": "2.0.0",
+	"tasks": [
+		{
+			"type": "cppbuild",
+			"label": "C/C++: g++ build active file",
+			"command": "/usr/bin/g++",
+			"args": [
+				"-fdiagnostics-color=always",
+				"-std=c++11",
+				"-g",
+				"${file}",
+				"-o",
+				"${fileDirname}/${fileBasenameNoExtension}"
+			],
+			"options": {
+				"cwd": "${fileDirname}"
+			},
+			"problemMatcher": [
+				"$gcc"
+			],
+			"group": "build",
+			"detail": "compiler: /usr/bin/g++"
+		},
+		{
+			"type": "2",
+			"label": "2",
+			"command": "2",
+			"args": [
+				"-std=c++11",
+				"${fileDirname}/${fileBasenameNoExtension}"
+			],
+			"options": {
+				"cwd": "${fileDirname}"
+			},
+			"problemMatcher": [
+				"$gcc"
+			],
+			"group": "build",
+			"detail": "compiler: /usr/bin/g++"
+		}
+	]
+}
+)"_json;
+    auto d = j2.dump(4);
+    printf("%s", d.c_str());
+    return 0;
+}
