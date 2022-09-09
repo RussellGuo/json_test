2c03fbd28dedcb9d84a26c83a3ba80949acc091d
Russell Guo <russellguo@msn.com>
2022-09-09 15:45:22 +0800

    wchar_t regex example.
    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 
 int main(int argc, char *argv[])
 {
-	std::string src = R"(use __PATH__ "main.tdl";)";
-	std::smatch m;
-	std::string include_pattern = R"(\s*((use)|(\$\s*include))\s+(__PATH__)?\s*\"(.*)\"\s*;?\s*)";
-	auto ret = std::regex_match(src, m, std::regex(include_pattern));
+	std::wstring src = LR"(#include __PATH__ "main.cpp";)";
+	std::wsmatch m;
+	std::wstring include_pattern = LR"(#\s*include\s+(__PATH__)?\s*\"(.*)\"\s*;?\s*)";
+	auto ret = std::regex_match(src, m, std::wregex(include_pattern));
 	if (ret) {
-		assert(m.size() > 5);
-		std::string path = m[4].str();
-		std::string file  = m[5].str();
-		std::cout << "path: '" << path <<"', file: '" << file << "'" << std::endl;
+		assert(m.size() > 2);
+		std::wstring path = m[1].str();
+		std::wstring file = m[2].str();
+		std::wcout << L"path: '" << path << L"', file: '" << file << L"'" << std::endl;
 	}
 
 	return 0;
