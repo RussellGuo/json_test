43b3043e6c38cff0b0b22768e7ea4359cd19d2b1
Russell Guo <russellguo@msn.com>
2022-12-14 15:29:43 +0800

    std::set test.
    .

--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,41 @@
 #include <assert.h>
 #include <iostream>
 #include <memory>
+#include <set>
 
 #include "json.hpp"
 
-size_t idx;
-struct str {
-    str() {
-        std::cerr << "created " << n << "\n";
-    }
-    str(const str&s): map(s.map) {
-        std::cerr << "duplicated " << n << "\n";
-    }
-    str(str&&s):map(std::move(s.map)){
-        std::cerr << "moved " << n << "\n";
-    }
-    ~str() {
-        std::cerr << "released " << n <<" len = " << map.size() << "\n";
-    }
-    const size_t n = idx++;
-    std::map<const char *, int>map;
+struct value_t {
+    const int value;
+    const std::string name;
+    value_t(int v, const std::string &_name): value(v), name(_name) {}
 };
 
-struct value_t {
-    str s;
-};
+static inline bool operator < (const value_t &v1, const value_t &v2) {
+    bool ret = v1.value < v2.value;
+    return ret;
+}
+static inline bool operator < (const value_t &v1, int v2) {
+    bool ret = v1.value < v2;
+    return ret;
+}
+static inline bool operator < (int v1, const value_t &v2) {
+    bool ret = v1 < v2.value;
+    return ret;
+}
+
+using value_set_t = std::set<value_t, std::less<>>;
 
 int main(int argc, char *argv[])
 {
-    str s1;
-    s1.map["char"] = 1;
-    s1.map["short"] = 2;
-    auto p = std::make_shared<const value_t, value_t>({.s = std::move(s1)});
-    p = nullptr;
+    value_set_t v_set;
+    const auto r1 = v_set.emplace(1, "A");
+    const auto r2 = v_set.emplace(2, "B");
+    const auto r3 = v_set.emplace(2, "A");
+    const auto r4 = v_set.find(2);
+    const auto r5 = v_set.find(4);
+    const auto r6 = r5 == v_set.end();
+
+
     return 0;
 }
