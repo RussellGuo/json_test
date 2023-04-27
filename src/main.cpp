d789c63de9b32c1ac8a7aca959ba9bcddcdca05a
Russell Guo <russellguo@msn.com>
2023-04-27 13:39:39 +0800

    sqlite demo.
    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,134 @@
+#include <assert.h>
+#include <sqlite3.h>
 #include <stdio.h>
 
-#include <regex>
-#include <assert.h>
 #include <iostream>
 #include <memory>
+#include <regex>
 #include <set>
 
-struct value_t {
-    const int value;
-    const std::string name;
-    value_t(int v, const std::string &_name): value(v), name(_name) {}
-};
+#include "json.hpp"
+#include "time_used.hxx"
 
-static inline bool operator < (const value_t &v1, const value_t &v2) {
-    bool ret = v1.value < v2.value;
-    return ret;
-}
-static inline bool operator < (const value_t &v1, int v2) {
-    bool ret = v1.value < v2;
-    return ret;
-}
-static inline bool operator < (int v1, const value_t &v2) {
-    bool ret = v1 < v2.value;
-    return ret;
-}
+int loadOrSaveDb(sqlite3 *pInMemory, const char *zFilename, int isSave) {
+    int rc;                  /* Function return code */
+    sqlite3 *pFile;          /* Database connection opened on zFilename */
+    sqlite3_backup *pBackup; /* Backup object used to copy data */
+    sqlite3 *pTo;            /* Database to copy to (pFile or pInMemory) */
+    sqlite3 *pFrom;          /* Database to copy from (pFile or pInMemory) */
 
-using value_set_t = std::set<value_t, std::less<>>;
+    /* Open the database file identified by zFilename. Exit early if this fails
+    ** for any reason. */
+    rc = sqlite3_open(zFilename, &pFile);
+    if (rc == SQLITE_OK) {
+        /* If this is a 'load' operation (isSave==0), then data is copied
+        ** from the database file just opened to database pInMemory.
+        ** Otherwise, if this is a 'save' operation (isSave==1), then data
+        ** is copied from pInMemory to pFile.  Set the variables pFrom and
+        ** pTo accordingly. */
+        pFrom = (isSave ? pInMemory : pFile);
+        pTo = (isSave ? pFile : pInMemory);
 
-namespace const_str_pool {
-using const_str = const char *;
-
-struct const_str_less_t {
-    bool operator () (const_str a, const_str b) const {
-        auto ret = strcmp(a, b);
-        return ret < 0;
-    }
-};
-
-std::set<const_str, const_str_less_t>pool;
-const_str mk_str(const_str str)
-{
-    auto ret = pool.find(str);
-    if (ret == pool.end()) {
-        const_str new_str = strdup(str);
-        if (new_str == nullptr) {
-            throw std::bad_alloc();
+        /* Set up the backup procedure to copy from the "main" database of
+        ** connection pFile to the main database of connection pInMemory.
+        ** If something goes wrong, pBackup will be set to NULL and an error
+        ** code and message left in connection pTo.
+        **
+        ** If the backup object is successfully created, call backup_step()
+        ** to copy data from pFile to pInMemory. Then call backup_finish()
+        ** to release resources associated with the pBackup object.  If an
+        ** error occurred, then an error code and message will be left in
+        ** connection pTo. If no error occurred, then the error code belonging
+        ** to pTo is set to SQLITE_OK.
+        */
+        pBackup = sqlite3_backup_init(pTo, "main", pFrom, "main");
+        if (pBackup) {
+            (void)sqlite3_backup_step(pBackup, -1);
+            (void)sqlite3_backup_finish(pBackup);
         }
-        pool.insert(new_str);
-        return new_str;
-    } else {
-        return *ret;
+        rc = sqlite3_errcode(pTo);
     }
 
+    /* Close the database connection opened on database file zFilename
+    ** and return the result of this function. */
+    (void)sqlite3_close(pFile);
+    return rc;
 }
 
-};
+int main(int, char *[]) {
+    time_used_t total("total");
+    time_used_t prepair("prepair");
+    sqlite3 *db;
+    if (sqlite3_open_v2(":memory:", &db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr)) {
+        printf("Could not open the.db\n");
+        exit(-1);
+    }
 
-int main(int, char *[])
-{
-    auto r1  = const_str_pool::mk_str("1");
-    auto r2  = const_str_pool::mk_str("2");
-    auto r1_ = const_str_pool::mk_str("1");
-    auto r2_ = const_str_pool::mk_str("2");
-    (void)r1;
-    (void)r2;
-    (void)r1_;
-    (void)r2_;
-    
+    if (sqlite3_exec(db, "create table tab(no integer, name text, age integer)", NULL, NULL, NULL)) {
+        printf("Error executing sql statement\n");
+    } else {
+        printf("Table created\n");
+    }
+
+    {
+        auto ret = sqlite3_exec(db, "PRAGMA synchronous = off", NULL, NULL, NULL);
+        printf("%d\n", ret);
+    }
+    //
+    // Prepare a statement for multiple use:
+    //
+    sqlite3_stmt *stmt;
+    if (sqlite3_prepare_v2(db, "insert into tab values(?, ?, ?)", -1, &stmt, NULL)) {
+        printf("Error executing sql statement\n");
+        exit(-1);
+    }
+
+    prepair.print();
+    time_used_t insert("insert");
+    for (int count = 0; count < 30 * 1000 * 1000; count++) {
+        if (count % 1000000 == 0) {
+            printf("%d\n", count);
+        }
+        //
+        // Bind the values for the first insert:
+        //
+        sqlite3_bind_int(stmt, 1, count + 1);
+        sqlite3_bind_text(stmt, 2, "three", -1, NULL);
+        sqlite3_bind_int(stmt, 3, 333);
+
+        //
+        // Do the first insert:
+        //
+        sqlite3_step(stmt);
+
+        //
+        // Reset the prepared statement to the initial state.
+        // This seems to be necessary in order to
+        // use the prepared statement again for another
+        // insert:
+        //
+        sqlite3_reset(stmt);
+    }
+
+    //
+    // Get rid of the memory allocated for stmt:
+    //
+    sqlite3_finalize(stmt);
+
+    insert.print();
+    time_used_t index("index");
+
+    {
+        auto ret = sqlite3_exec(db, "create index main_index on tab(no)", NULL, NULL, NULL);
+        printf("%d\n", ret);
+    }
+
+    index.print();
+
+    time_used_t backup("backup");
+    loadOrSaveDb(db, "test.db", true);
+    backup.print();
+    sqlite3_close(db);
+    total.print();
     return 0;
 }
