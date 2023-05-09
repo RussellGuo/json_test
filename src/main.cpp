7af1eb6edb2113f950a4ec487bef41106b72aeb3
Russell Guo <russellguo@msn.com>
2023-05-09 11:26:16 +0800

    json performance test.
    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <assert.h>
 #include <stdio.h>
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <memory>
 #include <regex>
 #include <set>
@@ -10,99 +10,63 @@
 #include "json.hpp"
 #include "time_used.hxx"
 
+constexpr int TIMES = 1000000;
+
 using json = nlohmann::json;
+int main(int, char *[]) {
+    struct txt_num_t {
+        const std::string txt;
+        const int num;
+    };
+    std::vector<txt_num_t> txt_num_table{
+        {"zero", 0},
+        {"one", 1},
+        {"two", 2},
+        {"three", 3},
+        {"four", 4},
+        {"five", 5},
+        {"six", 6},
+        {"seven", 7},
+        {"eight", 8},
+        {"night", 9},
+    };
 
-static json get_json_from_file(const std::string & file_name)
-{
-    std::ifstream f(file_name);
-    json j;
-    f >> j;
-    f.close();
-    return j;
-}
-
-struct json_statment_t {
-    const std::string var_name;
-    const std::string statment;
-};
-
-static int count = 0;
-std::string get_var_name()
-{
-    std::string name = "j" + std::to_string(count++);
-    return name;
-}
-
-static json_statment_t get_cpp_statment_of_json(const json &j) {
-    std::string name;
-    std::string statment;
-    bool is_array = j.is_array();
-    bool is_int = j.is_number_integer();
-    bool is_unsigned = j.is_number_unsigned();
-    bool is_float = j.is_number_float();
-    bool is_string = j.is_string();
-    bool is_object = j.is_object();
-
-    if (is_object) {
-        name = get_var_name();
-        std::string sub_item_string, sub_item_append;
-        for (const auto &item:j.items()) {
-            auto j_st = get_cpp_statment_of_json(item.value());
-            sub_item_string += j_st.statment;
-            sub_item_append += "    ret[\"" +item.key() + "\"] = " + j_st.var_name + ";\n";
+    time_used_t json_dump("json_dump");
+    for (int i = 0; i < TIMES; i++) {
+        json j_tab = "[]"_json;
+        for (const auto &[txt, num] : txt_num_table) {
+            json j;
+            j["txt"] = txt;
+            j["num"] = num;
+            j_tab.push_back((j));
         }
-        statment += sub_item_string + "static json " + name + "()\n{\n    json ret;\n" + sub_item_append + "\n    return ret;\n}\n\n";
-        name += "()";
-    } else if (is_array) {
-        name = get_var_name();
-        std::string sub_item_string, sub_item_append;
-        for (const auto &item:j) {
-            auto j_st = get_cpp_statment_of_json(item);
-            sub_item_string += j_st.statment;
-            sub_item_append += "    ret.push_back(" + j_st.var_name + ");\n";
+        auto s = j_tab.dump();
+
+        static bool printed;
+        if (!printed) {
+            std::cout << s << std::endl;
+            printed = true;
         }
-        statment += sub_item_string + "static json " + name + "()\n{\n    json ret;\n" + sub_item_append + "\n    return ret;\n}\n\n";
-        name += "()";
-    } else if (is_int) {
-        const std::string value_string = std::to_string(j.get<int>());
-        name = "(string_only_in_json ? json(\"" + value_string + "\") : json(" + value_string + ") )";
-    } else if (is_unsigned) {
-        const std::string value_string = std::to_string(j.get<unsigned>());
-        name = "(string_only_in_json ? json(\"" + value_string + "\") : json(" + value_string + ") )";
-    } else if (is_float) {
-        const std::string value_string = std::to_string(j.get<double>());
-        name = "(string_only_in_json ? json(\"" + value_string + "\") : json(" + value_string + ") )";
-    } else if (is_string) {
-        name = "json(\"" + j.get<std::string>() + "\")";
-    } else {
-        assert(false);
-        statment = "";
     }
+    json_dump.print();
 
-    return { name, statment };
-}
+    time_used_t str_cat("str_cat");
+    for (int i = 0; i < TIMES; i++) {
+        char buf[2048] = {0};
+        size_t l = 0;
+        l += sprintf(buf, "[");
+        for (const auto &[txt, num] : txt_num_table) {
+            l += sprintf(buf + l, R"({"num":%d,"txt":"%s"},)", num, txt.c_str());
+        }
+        sprintf(buf + l - 1, "]");
 
-
-int main(int argc, char *argv[]) {
-
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " xxx.json" << std::endl;
-        exit(1);
+        static bool printed;
+        if (!printed) {
+            fprintf(stderr, "%s\n", buf);
+            printed = true;
+        }
     }
-
-    json j = get_json_from_file(argv[1]);
-
-
-    auto ret = get_cpp_statment_of_json(j);
-
-    std::cout << ret.statment;
-    std::cout << "#define MAIN_JSON() " << ret.var_name << std::endl;
-
-#if 0
-    std::ofstream f("dump.json", std::ios::out);
-    f << j.dump(4);
-    f.close();
-#endif
+    str_cat.print();
 
     return 0;
 }
--- src/make_all.sh
+++ src/make_all.sh
@@ -1,40 +1,8 @@
 #!/bin/bash
 
-echo to gen mata code tools...
-g++ -O3 -o gen_code_from_json main.cpp
-
-echo to gen mata code...
-./gen_code_from_json 2038json.json > 2038json.inc
-
-echo to gen test json program...
-g++ -O3 -Wall -Wextra -o x86-test-json target_main.cpp
-
-echo to test json...
-./x86-test-json
-
-echo to gen test json program w/o exceptions...
-g++ -fno-exceptions -O3 -Wall -Wextra -o x86-test-json-ne target_main.cpp
-
-echo to test json w/o exceptions...
-./x86-test-json-ne
-
+g++ -O3 -Wall -Wextra -o x86-json-strcat main.cpp
 CROSS_GPP=aarch64-linux-gnu-g++-10
 
-echo to gen target test json programs...
-$CROSS_GPP -Wall -Wextra                                                                  -o arm-test-json-0 target_main.cpp
-$CROSS_GPP -Wall -Wextra -mfloat-abi=softfp -mfpu=vfp                                     -o arm-test-json-1 target_main.cpp
-$CROSS_GPP -Wall -Wextra -mfloat-abi=softfp -mfpu=neon                                    -o arm-test-json-2 target_main.cpp
-$CROSS_GPP -Wall -Wextra -mcpu=cortex-a8 -march=armv7-a -mfloat-abi=softfp -mfpu=vfp      -o arm-test-json-3 target_main.cpp
-$CROSS_GPP -Wall -Wextra -mcpu=cortex-a8 -march=armv7-a -mfloat-abi=softfp -mfpu=neon     -o arm-test-json-4 target_main.cpp
-
-$CROSS_GPP -O3 -Wall -Wextra                                                              -o arm-test-json-5 target_main.cpp
-$CROSS_GPP -O3 -Wall -Wextra -mfloat-abi=softfp -mfpu=vfp                                 -o arm-test-json-6 target_main.cpp
-$CROSS_GPP -O3 -Wall -Wextra -mfloat-abi=softfp -mfpu=neon                                -o arm-test-json-7 target_main.cpp
-$CROSS_GPP -O3 -Wall -Wextra -mcpu=cortex-a8 -march=armv7-a -mfloat-abi=softfp -mfpu=vfp  -o arm-test-json-8 target_main.cpp
-$CROSS_GPP -O3 -Wall -Wextra -mcpu=cortex-a8 -march=armv7-a -mfloat-abi=softfp -mfpu=neon -o arm-test-json-9 target_main.cpp
-
-for f in arm-test-json-*; do
-    aarch64-linux-gnu-strip -s $f
-done
+$CROSS_GPP -O3 -Wall -Wextra -o arm-json-strcat main.cpp
 
 echo All done!
