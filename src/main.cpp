8862ace0a871c7b52eda9f94b8ae6d1d90430ad5
Russell Guo <russellguo@msn.com>
2023-06-09 15:59:47 +0800

    protobuf learning.
    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,8 @@
 #include <regex>
 #include <set>
 
-#include "json.hpp"
-#include "time_used.hxx"
 #include "txt_num.pb.h"
 
-constexpr int TIMES = 1000000;
-
-using json = nlohmann::json;
 int main(int, char *[]) {
     struct txt_num_t {
         const std::string txt;
@@ -32,64 +27,35 @@ int main(int, char *[]) {
         {"night", 9},
     };
 
-    time_used_t pb("protobuf");
-    for (int i = 0; i < TIMES; i++) {
-        txt_num_tab txt_num_tab_pb;
-        for (const auto &[txt, num] : txt_num_table) {
-            auto item = txt_num_tab_pb.add_tab();
-            item->set_txt(txt);
-            item->set_num(num);
-        }
-        char buf[600] = {0};
-        bool ret = txt_num_tab_pb.SerializeToArray(buf, sizeof buf);
-        if (!ret) {
-            fprintf(stderr, "protobuf error\n");
-            exit(1);
-        }
-        static bool printed;
-        if (!printed) {
-            std::cout << "protobuf translated byte count: " << txt_num_tab_pb.ByteSizeLong() << std::endl;
-            printed = true;
-        }
+    txt_num_tab txt_num_tab_to_pb;
+    for (const auto &[txt, num] : txt_num_table) {
+        auto item = txt_num_tab_to_pb.add_tab();
+        item->set_txt(txt);
+        item->set_num(num);
     }
-    pb.print();
-
-    time_used_t json_dump("json_dump");
-    for (int i = 0; i < TIMES; i++) {
-        json j_tab = "[]"_json;
-        for (const auto &[txt, num] : txt_num_table) {
-            json j;
-            j["txt"] = txt;
-            j["num"] = num;
-            j_tab.push_back((j));
-        }
-        auto s = j_tab.dump();
-
-        static bool printed;
-        if (!printed) {
-            std::cout << s << std::endl;
-            printed = true;
-        }
+    char buf[600] = {0};
+    size_t buf_content_len;
+    bool ret = txt_num_tab_to_pb.SerializeToArray(buf, sizeof buf);
+    if (!ret) {
+        fprintf(stderr, "protobuf error\n");
+        exit(1);
     }
-    json_dump.print();
+    buf_content_len = txt_num_tab_to_pb.ByteSizeLong();
+    std::cout << "protobuf translated byte count: " << buf_content_len << std::endl;
 
-    time_used_t str_cat("str_cat");
-    for (int i = 0; i < TIMES; i++) {
-        char buf[600] = {0};
-        size_t l = 0;
-        l += sprintf(buf, "[");
-        for (const auto &[txt, num] : txt_num_table) {
-            l += sprintf(buf + l, R"({"num":%lf,"txt":"%s"},)", num, txt.c_str());
-        }
-        sprintf(buf + l - 1, "]");
-
-        static bool printed;
-        if (!printed) {
-            fprintf(stderr, "%s\n", buf);
-            printed = true;
-        }
+    txt_num_tab pb_to_txt_num_tab;
+    auto is_ok = pb_to_txt_num_tab.ParseFromArray(buf, buf_content_len);
+    if (!is_ok) {
+        fprintf(stderr, "protobuf decoding error\n");
+        exit(1);
     }
-    str_cat.print();
+    auto size = pb_to_txt_num_tab.tab_size();
+    for (int i = 0; i < size; i++) {
+        const auto &item = pb_to_txt_num_tab.tab(i);
+        const auto txt = item.txt();
+        const auto num = item.num();
+        std::cout << "txt: " << txt << " num: " << num << std::endl;
+    }
 
     return 0;
 }
