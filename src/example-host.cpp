72c4d5180788e8cebee6beaf0f6163dd0ae387b8
Guo Qiang <guoqiang@huaqin.com>
2023-07-25 17:32:05 +0800

    完善了C++/C各自的的protobuf编解码的例子
    CMakeLists.txt里写了主要用法
    remote_message.proto和 .options描述了消息格式
    .cpp .c俩文件则是程序示例，分别属于主机端和MCU端
    
    Change-Id: Ib54037e7018ead6fc992b6214d23bc96dcd052a9

--- src/example-host.cpp
+++ src/example-host.cpp
@@ -9,33 +9,67 @@
 
 #include "remote_message.pb.h"
 
+// C++ protobuf示例。郭强，2023-7-25
+
+// 编码示例，输入login_req，输出byte序列。出错时候序列为空
+inline static std::vector<uint8_t> login_req_encoding(const login_req &req) {
+    unsigned char req_buf[600] = {0};                          // 预计的size，一般不会超过
+    bool ret = req.SerializeToArray(req_buf, sizeof req_buf);  // 重点函数
+    if (ret) {
+        auto req_buf_len = req.ByteSizeLong();
+        return std::vector<uint8_t>(req_buf, req_buf + req_buf_len);  // 转换之
+    } else {
+        return std::vector<uint8_t>();  // 出错，返回空
+    }
+}
+
+// 解码示例，传入bye序列，输出login_req.
+// 输入：byte_data, 编码后的二进制数据
+// 输出：res, 解码后的数据存放处
+// 返回值：true代表正确
+inline static bool login_res_decoding(const std::vector<uint8_t> &byte_data, login_res &res) {
+    // 创建流
+    google::protobuf::io::CodedInputStream stream(byte_data.data(), byte_data.size());
+    // 解码
+    auto is_ok = res.ParseFromCodedStream(&stream);
+    // 还要判断一下是否都吃掉了（我们假设输入数据单纯是login_res的，没有尾巴，没有多余字段
+    auto pos = stream.CurrentPosition();
+    auto all_bytes_consumed = size_t(pos) == byte_data.size();
+    // 俩条件都要满足
+    return is_ok && all_bytes_consumed;
+}
+
 int main(int, char *[]) {
+    // 这是固定用法，检查.so库和 .exe 是否匹配
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    // 准备请求数据
     login_req req;
     req.set_username("Russell");
     req.set_password("12345");
-
-    char req_buf[600] = {0};
-    size_t req_buf_len;
-    bool ret = req.SerializeToArray(req_buf, sizeof req_buf);
-    if (!ret) {
-        std::cerr << "protobuf error" << std::endl;
+    // 获得串行化数据
+    auto login_req_data = login_req_encoding(req);
+    if (login_req_data.empty()) {
+        // error
+        std::cerr << "login_in_req encoding error" << std::endl;
         exit(1);
     }
-    req_buf_len = req.ByteSizeLong();
-    std::cerr << "protobuf translated byte count: " << req_buf_len << std::endl;
+    std::cerr << "protobuf translated byte count: " << login_req_data.size() << std::endl;
 
-    // pre-built buf here
-    const char expected_buf[] = {0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
-    const bool encoding_result = memcmp(req_buf, expected_buf, sizeof expected_buf) == 0 && req_buf_len == sizeof expected_buf;
-    std::cerr << "encoding result: " << encoding_result << std::endl;
+    // 应该当是这个结果。注意这个串会拿到MCU端解码。目前这部分是我手工复制过去的。
+    const std::vector<uint8_t> expected_encoding_result{0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    std::cerr << "encoding result: " << (login_req_data == expected_encoding_result) << std::endl;
 
-    const char res_buf[] = {8, 2};
+    //
+    // 解码示例，res编码后就俩字节
+    const std::vector<uint8_t> res_buf{8, 2};
     login_res res;
-    auto is_ok = res.ParseFromArray(res_buf, sizeof res_buf);
+    auto is_ok = login_res_decoding(res_buf, res);
     if (!is_ok) {
         std::cerr << "protobuf decoding error" << std::endl;
         exit(1);
     }
+    std::cerr << "login_res.status() is " << res.status() << std::endl;
 
     return 0;
 }
--- src/example-mcu.c
+++ src/example-mcu.c
@@ -1,26 +1,43 @@
 #include <assert.h>
-#include <stdio.h>
 
+// 在MCU端用nonopb编解码的示例，郭强，2023-7-25
+
+// 这俩是固定用法
 #include "pb_decode.h"
 #include "pb_encode.h"
+// 这是根据remote_message.proto 和 .options 生成的头文件
 #include "remote_message.pb.h"
 
-int main() {
-    login_req req = login_req_init_zero;
+int main(void) {
+    // 对req_buf解码。这组数据是我手工复制过来的，从C++的编码输出
     const unsigned char req_buf[] = {0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    // 这个解码辅助steam较为适合我们使用，因为MCU获得数据后去解码，是一段二进制数据。而且我们不光要解码，还要知道解码是否“消耗了”全部输入。如果不是代表出问题了。
     pb_istream_t in_stream = pb_istream_from_buffer(req_buf, sizeof req_buf);
+    // 存放解码结果的，先清空
+    login_req req = login_req_init_zero;
+    // 正式解码
     bool status = pb_decode(&in_stream, login_req_fields, &req);
+
+    // 用assert来观察输出。需要用debug模式来运行本程序，因为release版会忽略assert
     assert(status);
     assert(strcmp(req.username, "Russell") == 0);
     assert(strcmp(req.password, "12345") == 0);
     assert(in_stream.bytes_left == 0);
 
+    // nanopb的编码样例程序
+    // 准备数据
     login_res res = {.status = boolean_t_unknown};
+    // 准备buf
     unsigned char res_buf[600] = {0};
+    // 准备输出stream，其实就是输出到buf中
     pb_ostream_t out_stream = pb_ostream_from_buffer(res_buf, sizeof res_buf);
+    // 编码
     status = pb_encode(&out_stream, login_res_fields, &res);
+    // 检查返回值，同上面的一组assert
     assert(status);
+    // 运行观察过，输出结果是 {8 2} 序列。C++的解码程序也被证明是认得的。
     assert(out_stream.bytes_written == 2);
+
     assert(res_buf[0] == 8 && res_buf[1] == 2);
 
     return !status;
