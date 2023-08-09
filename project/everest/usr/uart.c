01018b29c627ab837af482d35ee1330fd7b72851
100107737 <caomeng6@huaqin.com>
2023-08-09 10:39:56 +0800

    移植初版uart驱动
    1.将saip里的驱动移植到everest_mcu上用于Android和MCU的通讯
    2.保持对上接口一致
    3.目前代码未做修改与适配
    
    Change-Id: I50d45a05af44c8799d4d877a5100320b901df964

--- project/everest/usr/uart.c
+++ project/everest/usr/uart.c
@@ -1,38 +1,280 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "uart.h"
-#include "mhscpu.h"
+// By Guo Qiang, XBD, Huaqin, 2020-11-19
+// a interrupt dirven UART low-level driver
 
-#define RECV_INT  (BIT0)
-#define SEND_INT  (BIT1 | BIT7)
+#include "uart_io_api.h"
 
-void uart_Config(uint32_t baudrate)
+#include "cmsis_os2.h"
+#include "gd32e10x.h"
+
+#include <string.h>
+#include "run_info_result_desc.h"
+
+#include "mcu-hw-common.h"
+
+#define UART2_TX_PIN                 GPIO_PIN_10
+#define UART2_RX_PIN                 GPIO_PIN_11
+#define UART2_GPIO_PORT              GPIOB
+#define UART2_GPIO_CLK               RCU_GPIOB
+
+#define UART0_TX_PIN                 GPIO_PIN_9
+#define UART0_RX_PIN                 GPIO_PIN_10
+#define UART0_GPIO_PORT              GPIOA
+#define UART0_GPIO_CLK               RCU_GPIOA
+
+
+#define USE_UART0
+
+#define USART                       USART0
+#define USART_IRQn                  USART0_IRQn
+
+#define UART_CLK                    RCU_USART0
+#define UART_TX_PIN                 UART0_TX_PIN
+#define UART_RX_PIN                 UART0_RX_PIN
+#define UART_GPIO_PORT              UART0_GPIO_PORT
+#define UART_GPIO_CLK               UART0_GPIO_CLK
+
+
+#define MAX_BYTE_RECV 136 // same as max datagram size
+#define MAX_BYTE_SEND 136 // same as max datagram size
+
+// A transmitted byte occupies the size of MQ.
+// I try it and found it's 16.
+#define MQ_DATAITEM_SIZE 16
+
+__ALIGNED(8) static uint8_t mem_of_mq_recv[MAX_BYTE_RECV * MQ_DATAITEM_SIZE];
+const static osMessageQueueAttr_t mq_attr_uart_recv = {
+    .name = "mq_uart_recv",
+    .mq_mem = mem_of_mq_recv,
+    .mq_size = sizeof mem_of_mq_recv,
+};
+
+__ALIGNED(8) static uint8_t mem_of_mq_send[MAX_BYTE_SEND * MQ_DATAITEM_SIZE];
+const static osMessageQueueAttr_t mq_attr_uart_send = {
+    .name = "mq_uart_send",
+    .mq_mem = mem_of_mq_send,
+    .mq_size = sizeof mem_of_mq_send,
+};
+
+// a message quque for uart receiving buffer, ISR will save data into it, and recv function will get data from it
+static osMessageQueueId_t mq_id_uart_recv;
+// a message quque for uart sending buffer, ISR will gey data from it, and send function will save data into it
+static osMessageQueueId_t mq_id_uart_send;
+
+#define SEND_QUEUE_DELIVERY_FLAG 2
+const static osEventFlagsAttr_t evt_flags_attr_of_sending_queue_delivery = {
+    .name = "evt_flags_of_sending_queue_changed",
+};
+
+static osEventFlagsId_t evt_flags_id_of_sending_queue_delivery; // ISR notifies sending function
+
+
+// a mutex for sending
+static osMutexId_t mutex_uart_sending_id;
+static const osMutexAttr_t mutex_uart_sending_attr = {
+    .name = "uart_sending_mutex",                          // human readable mutex name
+    .attr_bits = osMutexRecursive | osMutexPrioInherit,    // attr_bits
+};
+
+// pin define for UART
+static void init_uart_pins_uart(void);
+// controller configuration for UART
+static void init_uart_controller(uint32_t uart_no, uint8_t uart_irq);
+
+// init the UART
+// parameters: NONE
+// return value:
+//   true if done; otherwise failed
+bool init_uart_io_api(void)
 {
-    UART_InitTypeDef uart;
+    mq_id_uart_recv = osMessageQueueNew(MAX_BYTE_RECV, sizeof(uint8_t), &mq_attr_uart_recv);
+    mq_id_uart_send = osMessageQueueNew(MAX_BYTE_SEND, sizeof(uint8_t), &mq_attr_uart_send);
+    evt_flags_id_of_sending_queue_delivery = osEventFlagsNew(&evt_flags_attr_of_sending_queue_delivery);
+    mutex_uart_sending_id = osMutexNew (&mutex_uart_sending_attr);
 
-    uart.UART_BaudRate = baudrate;
-    uart.UART_Parity = UART_Parity_No;
-    uart.UART_StopBits = UART_StopBits_1;
-    uart.UART_WordLength = UART_WordLength_8b;
-    UART_Init(UART0, &uart);
-
-    GPIO_PinRemapConfig(GPIOA, GPIO_Pin_0 | GPIO_Pin_1, GPIO_Remap_0);
-
-    //Set Enable FIFO Receive Threshold 1/2 Send Threshold 2 characters in the FIFO
-    UART0->OFFSET_8.FCR = (2 << 6) | (1 << 4) | BIT2 | BIT1 | BIT0;
-    UART_ITConfig(UART0, RECV_INT, ENABLE);
-    NVIC_EnableIRQ(UART0_IRQn);
-}
-
-int fputc(int ch, FILE *f)
-{
-    /* Place your implementation of fputc here */
-    /* e.g. write a character to the USART */
-    if (ch == '\n')
-    {
-        fputc('\r', f);
+    bool ret = mq_id_uart_recv != NULL && mq_id_uart_send != NULL && evt_flags_id_of_sending_queue_delivery != NULL && mutex_uart_sending_id != NULL;
+    if (ret) {
+        init_uart_pins_uart();
+        init_uart_controller(USART, USART_IRQn);
     }
-    while(!UART_IsTXEmpty(UART0));
-    UART_SendData(UART0, (uint8_t) ch);
-    return ch;
+
+    return ret;
 }
+
+// receive one byte from UART
+// parameters:
+//  [out]byte, ptr to store the received byte
+//  [in] delay, max time tick before the operation
+// return value:
+//   true if done; otherwise failed
+bool uart_recv_byte(uint8_t *byte, const uint32_t delay)
+{
+    osStatus_t status;
+    for(;;) {
+        status = osMessageQueueGet(mq_id_uart_recv, byte, NULL, delay);
+        if (status != osErrorResource) {
+            break;
+        }
+
+        // Temporarily unable to read the data, then wait and try again
+        osDelay(1);
+    }
+    return status == osOK;
+}
+
+// send data into UART
+// parameters:
+//  [in]buf, ptr to store the data
+//  [in]size, size of the 'buf'
+//  [in]delay, max time tick before the operation
+// return value:
+//   true if done; otherwise failed
+bool uart_send_data(const uint8_t *buf, size_t size, const uint32_t delay)
+{
+    (void)delay;
+
+    if (buf == NULL) {
+        return false;
+    }
+    if (size == 0) {
+        return true;
+    }
+
+    if (osMessageQueueGetCapacity(mq_id_uart_send) < size) {
+        return false;
+    }
+
+    bool ret = false;
+    uint32_t begin_tick = osKernelGetTickCount();
+
+    if (osMutexAcquire(mutex_uart_sending_id, delay) != osOK) {
+        return false;
+    }
+
+    for(;;) {
+        if (osMessageQueueGetSpace(mq_id_uart_send) >= size) {
+            ret = true;
+            break;
+        }
+
+        // to calc the left delay time
+        uint32_t delay_from_now;
+        if (delay == osWaitForever) {
+            // forever
+            delay_from_now = osWaitForever;
+        } else {
+            uint32_t now = osKernelGetTickCount();
+            if (now >= begin_tick + delay) {
+                // already time out
+                ret = false;
+                break;
+            } else {
+                // a smaller delay time
+                delay_from_now = delay + begin_tick - now;
+            }
+        }
+        uint32_t evt_flags_wait_ret = osEventFlagsWait(evt_flags_id_of_sending_queue_delivery, SEND_QUEUE_DELIVERY_FLAG, osFlagsWaitAny, delay_from_now);
+        if (evt_flags_wait_ret == (uint32_t)osErrorTimeout) {
+            ret = false;
+            break;
+        }
+    }
+
+    if (ret) {
+        for (size_t i = 0; i < size; i++) {
+            osStatus_t status = osMessageQueuePut(mq_id_uart_send, buf + i, 0, 0);
+            if (status != osOK) {
+                // I think it's never be reached
+                ret = false;
+                break;
+            }
+        }
+        usart_interrupt_enable(USART, USART_INT_TBE);
+    }
+
+    osMutexRelease(mutex_uart_sending_id);
+    return ret;
+}
+
+
+static rcu_periph_enum rpu_tab[] = {
+    UART_GPIO_CLK,   // GPIO clock
+    UART_CLK,        // USART clock
+};
+
+static const struct mcu_pin_t uart_pin_tab[] = {
+    {UART_GPIO_PORT, UART_TX_PIN, GPIO_MODE_AF_PP      }, // USARTx_Tx
+    {UART_GPIO_PORT, UART_RX_PIN, GPIO_MODE_IN_FLOATING}, // USARTx_Rx
+};
+
+// pin define for UART
+static void init_uart_pins_uart(void)
+{
+    // power on the clock
+    enable_rcus(rpu_tab, sizeof(rpu_tab) / sizeof(rpu_tab[0]));
+
+    /* connect port to USARTx_Tx */
+    setup_pins(uart_pin_tab, sizeof(uart_pin_tab) / sizeof(uart_pin_tab[0]));
+}
+
+// controller configuration for UART
+static void init_uart_controller(uint32_t uart_no, uint8_t uart_irq)
+{
+    /* USART configure */
+    usart_deinit(uart_no);
+    usart_baudrate_set(uart_no, 115200U);
+    usart_receive_config(uart_no, USART_RECEIVE_ENABLE);
+    usart_transmit_config(uart_no, USART_TRANSMIT_ENABLE);
+    usart_enable(uart_no);
+
+    nvic_irq_enable(uart_irq, 0, 0);
+    usart_interrupt_enable(uart_no, USART_INT_RBNE);
+}
+
+
+static inline void uart_irq(uint32_t uart_no)
+{
+    // TODO: record_uart_recv_error(1) if Frame Error/Noise Error/overrun be catched
+
+    if(RESET != usart_interrupt_flag_get(uart_no, USART_INT_FLAG_RBNE)){
+        /* receive data */
+        uint8_t byte = (uint8_t)usart_data_receive(uart_no);
+        if (osMessageQueueGetSpace(mq_id_uart_recv) < 1) {
+                uint8_t dropped_byte;
+                osMessageQueueGet(mq_id_uart_recv, &dropped_byte, 0, 0);
+                record_uart_recv_dropped(1);
+        }
+        osStatus_t status = osMessageQueuePut(mq_id_uart_recv, &byte, 0, 0);
+        if (status != osOK) {
+            record_uart_inner_error(1);
+        } else {
+            record_uart_recv(1);
+        }
+
+    }
+    if(RESET != usart_interrupt_flag_get(uart_no, USART_INT_FLAG_TBE)){
+        /* transmit data */
+        uint8_t byte;
+        osStatus_t status = osMessageQueueGet(mq_id_uart_send, &byte, 0, 0);
+        if (status == osOK) {
+            uint32_t evt_set_ret = osEventFlagsSet(evt_flags_id_of_sending_queue_delivery, SEND_QUEUE_DELIVERY_FLAG);
+            evt_set_ret = 0;
+            usart_data_transmit(uart_no, byte);
+        } else {
+            usart_interrupt_disable(uart_no, USART_INT_TBE);
+        }
+    }
+}
+
+#if defined(USE_UART2)
+void USART2_IRQHandler(void)
+{
+    uart_irq(USART2);
+}
+#endif
+
+#if defined(USE_UART0)
+void USART0_IRQHandler(void)
+{
+    uart_irq(USART0);
+}
+#endif
