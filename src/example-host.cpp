609ac36fcb5ed8df1f34e6e6954ca9947c4b2fbe
Guo Qiang <guoqiang@huaqin.com>
2023-08-18 15:33:58 +0800

    pb中oneof的使用示例，C/C++版
    目前还缺少keil C的例子.
    
    Change-Id: I167dc5e18bfb5e54d4ff83f6ceca03e42ce5a7ac

--- src/example-host.cpp
+++ src/example-host.cpp
@@ -11,28 +11,30 @@
 
 // C++ protobuf示例。郭强，2023-7-25
 
-// 编码示例，输入login_req，输出byte序列。出错时候序列为空
-inline static std::vector<uint8_t> login_req_encoding(const login_req &req) {
-    unsigned char req_buf[600] = {0};                          // 预计的size，一般不会超过
-    bool ret = req.SerializeToArray(req_buf, sizeof req_buf);  // 重点函数
+// 一个总的编码函数，针对pb对象，生成其二进制数据（流化）
+template <typename T, size_t MAX_SIZE = 600>
+std::vector<uint8_t> pb_encoding(const T &pb_obj) {
+    unsigned char buf[MAX_SIZE] = {0};                    // 预计的size
+    bool ret = pb_obj.SerializeToArray(buf, sizeof buf);  // 重点函数
     if (ret) {
-        auto req_buf_len = req.ByteSizeLong();
-        return std::vector<uint8_t>(req_buf, req_buf + req_buf_len);  // 转换之
+        auto req_buf_len = pb_obj.ByteSizeLong();
+        return std::vector<uint8_t>(buf, buf + req_buf_len);  // 转换之
     } else {
         return std::vector<uint8_t>();  // 出错，返回空
     }
 }
 
-// 解码示例，传入bye序列，输出login_req.
+// 解码示例，传入bye序列，输出到pb_obj.
 // 输入：byte_data, 编码后的二进制数据
-// 输出：res, 解码后的数据存放处
+// 输出：pb_obj, 解码后的数据存放处
 // 返回值：true代表正确
-inline static bool login_res_decoding(const std::vector<uint8_t> &byte_data, login_res &res) {
+template <typename T>
+bool pb_decoding(const std::vector<uint8_t> &byte_data, T &pb_obj) {
     // 创建流
     google::protobuf::io::CodedInputStream stream(byte_data.data(), byte_data.size());
     // 解码
-    auto is_ok = res.ParseFromCodedStream(&stream);
-    // 还要判断一下是否都吃掉了（我们假设输入数据单纯是login_res的，没有尾巴，没有多余字段
+    auto is_ok = pb_obj.ParseFromCodedStream(&stream);
+    // 还要判断一下是否都吃掉了（我们假设输入数据单纯是pb_obj的，没有尾巴，没有多余字段
     auto pos = stream.CurrentPosition();
     auto all_bytes_consumed = size_t(pos) == byte_data.size();
     // 俩条件都要满足
@@ -44,32 +46,59 @@ int main(int, char *[]) {
     GOOGLE_PROTOBUF_VERIFY_VERSION;
 
     // 准备请求数据
-    login_req req;
-    req.set_username("Russell");
-    req.set_password("12345");
+    login_req login_req_obj;
+    login_req_obj.set_username("Russell");
+    login_req_obj.set_password("12345");
     // 获得串行化数据
-    auto login_req_data = login_req_encoding(req);
-    if (login_req_data.empty()) {
+    auto login_req_obj_binary = pb_encoding(login_req_obj);
+    if (login_req_obj_binary.empty()) {
         // error
-        std::cerr << "login_in_req encoding error" << std::endl;
+        std::cerr << "login_req encoding error" << std::endl;
         exit(1);
     }
-    std::cerr << "protobuf translated byte count: " << login_req_data.size() << std::endl;
+    std::cerr << "login_req: protobuf translated byte count: " << login_req_obj_binary.size() << std::endl;
 
     // 应该当是这个结果。注意这个串会拿到MCU端解码。目前这部分是我手工复制过去的。
-    const std::vector<uint8_t> expected_encoding_result{0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
-    std::cerr << "encoding result: " << (login_req_data == expected_encoding_result) << std::endl;
+    const std::vector<uint8_t> expected_login_encoding_binary{0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    std::cerr << "login_req: encoding result: " << (login_req_obj_binary == expected_login_encoding_binary) << std::endl;
 
     //
-    // 解码示例，res编码后就俩字节
-    const std::vector<uint8_t> res_buf{8, 2};
-    login_res res;
-    auto is_ok = login_res_decoding(res_buf, res);
+    // 解码示例，login_res编码后就俩字节
+    const std::vector<uint8_t> login_res_buf{8, 2};
+    login_res login_res_obj;
+    auto is_ok = pb_decoding(login_res_buf, login_res_obj);
     if (!is_ok) {
-        std::cerr << "protobuf decoding error" << std::endl;
+        std::cerr << "login_req: protobuf decoding error" << std::endl;
         exit(1);
     }
-    std::cerr << "login_res.status() is " << res.status() << std::endl;
+    std::cerr << "login_res.status() is " << login_res_obj.status() << std::endl;
+
+    // 示例oneof的用法
+    to_mcu to_mcu_req_obj;
+    to_mcu_req_obj.set_allocated_login(&login_req_obj);  // 这个写法是我找到的唯一能用的方法
+    to_mcu_req_obj.set_seq(0x1U);
+    to_mcu_req_obj.set_crc(0xffffU);
+    auto to_mcu_req_binary = pb_encoding(to_mcu_req_obj);
+    if (to_mcu_req_binary.empty()) {
+        // error
+        std::cerr << "to_mcu_req_obj encoding error" << std::endl;
+        exit(1);
+    }
+    to_mcu_req_obj.release_login();  // 如果不是new出来的指针，得尽快release掉，否则会在to_mcu_req的析构中被灾难性的delete. 此处的release不会调用delete
+    std::cerr << "to_mcu_req: protobuf translated byte count: " << to_mcu_req_binary.size() << std::endl;
+
+    // 应该当是这个结果。注意这个串会拿到MCU端解码。目前这部分是我手工复制过去的。
+    const std::vector<uint8_t> expected_to_mcu_req_encoding_binary{0x08, 0x01, 0x15, 0xff, 0xff, 0x00, 0x00, 0x1a, 0x10, 0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    std::cerr << "to_mcu_req encoding result: " << (to_mcu_req_binary == expected_to_mcu_req_encoding_binary) << std::endl;
+
+    // 解码from_mcu
+    // 跟普通解码没有不同。不同的是获取obj的值的方法要注意
+    const std::vector<uint8_t>from_mcu_res_binary {0x08, 0x02, 0x15, 0xfe, 0xff, 0x00, 0x00, 0x1a, 0x02, 0x08, 0x02};
+    from_mcu from_mcu_res_obj;
+    is_ok = pb_decoding(from_mcu_res_binary, from_mcu_res_obj);
+    is_ok &= from_mcu_res_obj.res_case() == from_mcu_res_obj.kLogin;
+    is_ok &= from_mcu_res_obj.login().status() == boolean_t::unknown;
+    std::cerr << "from_mcu_res: decoding result = " << is_ok << std::endl;
 
     return 0;
 }
--- src/example-mcu.c
+++ src/example-mcu.c
@@ -10,35 +10,63 @@
 
 int main(void) {
     // 对req_buf解码。这组数据是我手工复制过来的，从C++的编码输出
-    const unsigned char req_buf[] = {0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    const unsigned char login_req_buf[] = {0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
     // 这个解码辅助steam较为适合我们使用，因为MCU获得数据后去解码，是一段二进制数据。而且我们不光要解码，还要知道解码是否“消耗了”全部输入。如果不是代表出问题了。
-    pb_istream_t in_stream = pb_istream_from_buffer(req_buf, sizeof req_buf);
+    pb_istream_t in_stream = pb_istream_from_buffer(login_req_buf, sizeof login_req_buf);
     // 存放解码结果的，先清空
-    login_req req = login_req_init_zero;
+    login_req login_req_obj = login_req_init_zero;
     // 正式解码
-    bool status = pb_decode(&in_stream, login_req_fields, &req);
+    bool status = pb_decode(&in_stream, login_req_fields, &login_req_obj);
 
     // 用assert来观察输出。需要用debug模式来运行本程序，因为release版会忽略assert
     assert(status);
-    assert(strcmp(req.username, "Russell") == 0);
-    assert(strcmp(req.password, "12345") == 0);
+    assert(strcmp(login_req_obj.username, "Russell") == 0);
+    assert(strcmp(login_req_obj.password, "12345") == 0);
     assert(in_stream.bytes_left == 0);
 
     // nanopb的编码样例程序
     // 准备数据
-    login_res res = {.status = boolean_t_unknown};
+    login_res login_res_obj = {.status = boolean_t_unknown};
     // 准备buf
-    unsigned char res_buf[600] = {0};
+    unsigned char login_res_buf[600] = {0};
     // 准备输出stream，其实就是输出到buf中
-    pb_ostream_t out_stream = pb_ostream_from_buffer(res_buf, sizeof res_buf);
+    pb_ostream_t out_stream = pb_ostream_from_buffer(login_res_buf, sizeof login_res_buf);
     // 编码
-    status = pb_encode(&out_stream, login_res_fields, &res);
+    status = pb_encode(&out_stream, login_res_fields, &login_res_obj);
     // 检查返回值，同上面的一组assert
     assert(status);
     // 运行观察过，输出结果是 {8 2} 序列。C++的解码程序也被证明是认得的。
     assert(out_stream.bytes_written == 2);
 
-    assert(res_buf[0] == 8 && res_buf[1] == 2);
+    assert(login_res_buf[0] == 8 && login_res_buf[1] == 2);
+
+    // 来个复杂的解码例子：to_mcu. 这段数据也是复制来的。
+    const unsigned char to_mcu_buf[] = {0x08, 0x01, 0x15, 0xff, 0xff, 0x00, 0x00, 0x1a, 0x10, 0x0a, 0x07, 0x52, 0x75, 0x73, 0x73, 0x65, 0x6c, 0x6c, 0x12, 0x05, 0x31, 0x32, 0x33, 0x34, 0x35};
+    in_stream = pb_istream_from_buffer(to_mcu_buf, sizeof to_mcu_buf);
+    to_mcu to_mcu_obj = to_mcu_init_zero;
+    status = pb_decode(&in_stream, to_mcu_fields, &to_mcu_obj);
+
+    // 用assert来观察输出。需要用debug模式来运行本程序，因为release版会忽略assert
+    assert(status);
+    assert(to_mcu_obj.which_req == to_mcu_login_tag);
+    assert(strcmp(to_mcu_obj.req.login.username, "Russell") == 0);
+    assert(strcmp(to_mcu_obj.req.login.password, "12345") == 0);
+    assert(in_stream.bytes_left == 0);
+
+    // 构造一个编码的例子
+    from_mcu from_mcu_obj = from_mcu_init_zero;
+    from_mcu_obj.crc = 0xFFFEU;
+    from_mcu_obj.seq = 2U;
+    from_mcu_obj.which_res = from_mcu_login_tag;
+    from_mcu_obj.res.login = login_res_obj;
+    unsigned char from_mcu_buf[600] = {0};
+    out_stream = pb_ostream_from_buffer(from_mcu_buf, sizeof from_mcu_buf);
+    status = pb_encode(&out_stream, from_mcu_fields, &from_mcu_obj);
+    // 预期是这一串，我纯手敲的（debug方式运行到这里然后观察变量值，再抄下来）。这段会放在C++里解码
+    const unsigned char expected_from_mcu_binary[] = {0x08, 0x02, 0x15, 0xfe, 0xff, 0x00, 0x00, 0x1a, 0x02, 0x08, 0x02};
+    assert(status);
+    assert(out_stream.bytes_written == sizeof expected_from_mcu_binary);
+    assert(memcmp(from_mcu_buf, expected_from_mcu_binary, sizeof expected_from_mcu_binary) == 0);
 
     return !status;
 }
