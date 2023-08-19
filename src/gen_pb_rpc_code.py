529947b372ac256fd04b9c0a62d7cb86284378aa
Guo Qiang <guoqiang@huaqin.com>
2023-08-19 19:31:44 +0800

    自动生成MCU的语义层代码，未完成
    主要为不同的业务类型生成套路代码。自动生成省人工
    
    Change-Id: I286939a36b168acdc7c2717fadec546296a8ed05

--- src/gen_pb_rpc_code.py
+++ src/gen_pb_rpc_code.py
@@ -1,6 +1,94 @@
 import meta_info_from_pb
 
-print("req list:", meta_info_from_pb.req_list)
-print("res list:", meta_info_from_pb.res_list)
-print("evt list:", meta_info_from_pb.evt_list)
+req_list, res_list, evt_list = meta_info_from_pb.req_list, meta_info_from_pb.res_list, meta_info_from_pb.evt_list
 
+# 我们要生成代码啦！首先我们把请求响应对应的业务名提取出来，建立一个从func->(req信息，res信息)的元组
+rpc_func_map = {}
+# 下面的处理就是把类型名丢掉后缀当作func
+for item in req_list:
+    req = item[0][:-4]
+    rpc_func_map[req] = item
+# 请求好了，加上响应
+for item in res_list:
+    res = item[0][:-4]
+    rpc_func_map[res] = (rpc_func_map[res], item)
+
+evt_map = {}
+for item in evt_list:
+    evt = item[0][:-4]
+    evt_map[evt] = item
+
+del req_list, res_list, evt_list, item, req, res, evt
+
+# 先输出一下头文件。目前还不知道文件名/路径，先打印到终端看看
+print(
+    '''
+/* 别编辑，自动生成的代码
+   生成命令是：。。。。。
+ */
+#include "remote_message.pb.h"
+''')
+for func in rpc_func_map:
+    req_type_name, req_obj_name, req_number, req_type_is_dummy = rpc_func_map[func][0]
+    res_type_name, res_obj_name, res_number, res_type_is_dummy = rpc_func_map[func][1]
+    print("extern remote_call_err_code remote_call_service_for_%s(const %s *%s, %s *%s);" %
+          (func, req_type_name, req_obj_name, res_type_name, res_obj_name))
+    pass
+for evt in evt_map:
+    evt_type_name, evt_obj_name, evt_number, evt_type_is_dummy = evt_map[evt]
+    print("extern bool send_remote_event_for_%s(const %s *%s);" %
+          (evt, evt_type_name, evt_obj_name))
+    pass
+
+# 试一试C文件
+print(
+    '''
+/* 别编辑，自动生成的代码
+   生成命令是：。。。。。
+ */
+#include "上头那个.h"
+''')
+for func in rpc_func_map:
+    req_type_name, req_obj_name, req_number, req_type_is_dummy = rpc_func_map[func][0]
+    res_type_name, res_obj_name, res_number, res_type_is_dummy = rpc_func_map[func][1]
+    print('''
+__attribute__((weak)) remote_call_err_code remote_call_service_for_%s(const %s *%s_req_obj, %s *%s_res_obj) {
+    (void)%s_req_obj;
+    (void)%s_res_obj;
+    return remote_call_err_code_no_impl;
+}''' % (func, req_type_name, req_obj_name, res_type_name, res_obj_name, req_obj_name, res_obj_name))
+    pass
+
+for evt in evt_map:
+    evt_type_name, evt_obj_name, evt_number, evt_type_is_dummy = evt_map[evt]
+    print('''
+bool send_remote_event_for_%s(const %s *%s) {
+    bool ret;
+    from_mcu from_mcu_obj = from_mcu_init_zero;
+    from_mcu_obj.which_res = from_mcu_%s_tag;
+    from_mcu_obj.res.%s = *%s;
+    ret = send_remote_res(&from_mcu_obj);
+    return ret;
+}''' % (evt, evt_type_name, evt_obj_name, evt, evt_obj_name, evt_obj_name))
+
+print('''
+remote_call_err_code remote_call_service(const to_mcu *to_mcu_obj, from_mcu *from_mcu_obj) {
+    remote_call_err_code err_code;
+    switch(to_mcu_obj->which_req) {''')
+
+for func in rpc_func_map:
+    req_type_name, req_obj_name, req_number, req_type_is_dummy = rpc_func_map[func][0]
+    res_type_name, res_obj_name, res_number, res_type_is_dummy = rpc_func_map[func][1]
+    print('''
+    case to_mcu_%s_tag:
+        err_code = remote_call_service_for_%s(to_mcu_obj->req.%s, from_mcu_obj->req.%s);
+        break;
+'''%(func, func, req_obj_name, res_obj_name))
+
+print('''
+    default:
+        err_code = remote_call_err_code_req_type_err;
+        break;
+    }
+    return err_code;
+}''')
--- src/meta_info_from_pb.py
+++ src/meta_info_from_pb.py
@@ -1,27 +1,63 @@
+
+'''从项目自定义的protobuf文件中提取pb的符号信息
+
+ 在运行前，要执行：
+#    ../../nanopb/generator/protoc --python_out=. remote_message.proto
+来生成pb的python版本。随后我们就能获取所需的信息了
+
+我们的pb有如下约定： 
+    host->mcu的请求/响应消息有复数个
+        对于每个业务func而言，业务的请求消息命名为func_req，响应消息未func_res。
+        所有的请求消息会放在to_mcu这个消息中，所有的响应消息会放在from_mcu中
+        因为to_mcu/from_mcu可以承载各种func，所以会有 oneof 字段（名字分别是是req/res）
+    除了上面说的func的请求/响应func外，从mcu发出的event也是一种类别。这种则是_evt结尾的类型
+        evt类型也会出现在from_mcu的oneof字段中。具体可参见 remote_message.proto
+正因为上述约定，所以我们可以利用约定从生成的python程序中提取出来请求/响应/事件的列表
+三个列表，每个列表表项是个元组，包含类型名 成员对象名 内部编号 是否是哑类型
+这几个列表会用于自动生成Java/C的处理程序
+'''
+
 import remote_message_pb2
 
 
 def is_a_dummy_type(type_name):
+    '''针对一个类型，判断其是否是哑的
+    哑类型一定是自定义类型，其中只有一个字段dummy
+    这种类型相当于void，只是占位符'''
     try:
+        # 从内部实现中搜。或许跟具体的pb编译器版本有关
         type = remote_message_pb2.DESCRIPTOR.message_types_by_name[type_name]
         is_dummy = len(type.fields) == 1 and type.fields[0].name == 'dummy'
         return is_dummy
     except e:
         return False
 
+
 def get_field_list(fields_inside_pb, type_name_filter):
-    from_mcu_list = fields_inside_pb
+    '''
+    获取指定字段列表fields_inside_pb的每一个元素的信息，包括 类型名 对象名 编号 和是否哑类型
+    type_name_filter是针对类型名的真值过滤器
+    '''
     field_list = []
-    for field in from_mcu_list:
+    for field in fields_inside_pb:
+        # 针对每条信息
         type_name, field_name, number = field.message_type.name, field.name, field.number
+        # 过滤
         is_matched = type_name_filter(type_name)
         if not is_matched:
             continue
+        # 查出来是不是哑类型
         is_dummy = is_a_dummy_type(type_name)
+        # 填入元组
         field_list.append((type_name, field_name, number, is_dummy))
+    # 最后返回之
     return field_list
 
-evt_list = get_field_list(remote_message_pb2._FROM_MCU.oneofs_by_name['res'].fields, lambda type_name: type_name.endswith('_evt'))
-res_list = get_field_list(remote_message_pb2._FROM_MCU.oneofs_by_name['res'].fields, lambda type_name: type_name.endswith('_res'))
-req_list = get_field_list(remote_message_pb2._TO_MCU.oneofs_by_name['req'].fields, lambda type_name: type_name.endswith('_req'))
+#请看顶部注释，我们把相关信息放入全局变量中
+evt_list = get_field_list(
+    remote_message_pb2._FROM_MCU.oneofs_by_name['res'].fields, lambda type_name: type_name.endswith('_evt'))
+res_list = get_field_list(
+    remote_message_pb2._FROM_MCU.oneofs_by_name['res'].fields, lambda type_name: type_name.endswith('_res'))
+req_list = get_field_list(
+    remote_message_pb2._TO_MCU.oneofs_by_name['req'].fields, lambda type_name: type_name.endswith('_req'))
 pass
