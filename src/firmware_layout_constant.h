66f9bcc8fe4e2dc616f7571c1e00d55a95b9e78f
Guo Qiang <guoqiang@huaqin.com>
2023-12-01 10:03:55 +0800

    整理代码
    。

--- src/firmware_layout_constant.h
+++ src/firmware_layout_constant.h
@@ -1,13 +1,25 @@
 #ifndef __FIRMWARE_LAYOUT_CONSTANT_H__
 #define __FIRMWARE_LAYOUT_CONSTANT_H__
 
+/*
+    固件布局所用的常数
+    郭强 2023-11-30
+    在本案中，我们初步定下来固件的布局。整个固件最大尺寸暂定为2M。
+    签名的二进制数据放置在固件的0地址，长度位256个字节。
+    随后跟着一个4字节长度的“可执行体”的长度数据。这是Keil编译出来生成的.bin（注意不是hex）的长度
+    中间跳掉252字节，这252字节可以留作其它字段使用，比如版本号、版本信息等自定义内容
+    从512字节开始存放的就是“可执行体”本身了。
+    前面说的签名，针对的数据是包含了从4字节开始到执行体末尾的全部数据。也就是说长度信息、附加信息、可执行体均受签名保护
+    被签名的数据先经过SHA512算法变成固定64字节长度的摘要，摘要则通过RSA算法、指定的私钥来签出。保存在固件开头
+ */
+
 #define TOTAL_FIRMWARE_MAX_SIZE (2 * 1024 * 1024)                           // 整体firmware的最大尺寸
 #define SIGNATURE_POS           0                                           // 签名的位置
 #define SIGNATURE_SIZE          256                                         // 签名的大小，就是RSA2048的数据输入输出大小
 #define EXEC_LEN_ADDR           (SIGNATURE_POS + SIGNATURE_SIZE + 0)        // 可执行体的大小，放在firmware的这个偏移地址上
 #define EXEC_LEN_SIZE           4                                           // 长度是4字节。little-endian。
-#define EXEC_BLOCK_POS          512                                         // MCU的bin文件起始地址
-#define EXEC_BLOCK_MAX_SIZE     (TOTAL_FIRMWARE_MAX_SIZE - EXEC_BLOCK_POS)  // 执行体的最大尺寸
-#define BEGIN_POS_OF_PLAIN      EXEC_LEN_ADDR                               // 签名起始位置。 结束位置在EXEC_BLOCK_POS + exec长度
+#define EXEC_BODY_POS          512                                          // MCU的bin文件起始地址
+#define EXEC_BODY_MAX_SIZE     (TOTAL_FIRMWARE_MAX_SIZE - EXEC_BODY_POS)    // 执行体的最大尺寸
+#define BEGIN_POS_OF_PLAIN      EXEC_LEN_ADDR                               // 签名起始位置。 结束位置在EXEC_BODY_POS + exec长度
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,50 @@
+/*
+    加密机端签名程序
+    郭强 2023-11-30
+    签名程序是针对Keil（或者其它开发工具）生成的二进制“可执行体”加入签名等附加信息的程序。详情请见print_usage的描述
+ */
 #include <assert.h>
 #include <getopt.h>
 #include <stdio.h>
-#include <unistd.h>
 
 #include <fstream>
 #include <iostream>
-#include <vector>
 
 #include "firmware_layout_constant.h"
 #include "gen_signature.hpp"
 
-uint8_t firmware_memory[TOTAL_FIRMWARE_MAX_SIZE];
+// 最终生成的fireware的内存平面数据
+static uint8_t firmware_memory[TOTAL_FIRMWARE_MAX_SIZE];
 
-void print_usage(const char *argv0) {
-    fprintf(stderr, "Usage: %s -r private-key.pem -u public-key.pem -o output.bin -e executable.bin\n", argv0);
+/*
+    打印程序用法并退出，参数是本程序的可执行程序名。命令行选项中
+    -r 带出 RSA2048 的私钥，pem形式的文件。这个文件十分当紧，绝不可泄露
+    -u 带出 RSA2048 的共钥，pem形式的文件。这个文件是要共享给MCU端的。本程序会用这个公钥验证签名
+    -o 带出 输出的固件的二进制文件名，这个就是发行用的文件
+    -e 带出 Keil或其它“可执行体”的二进制形式。此文件要在Keil中用fromaxf生成（fromelf --bin --output=outfile.bin infile.axf）
+    -i 选项可选可不选，如果加上，还会生成一个 .inc输出文件，这个文件可以直接复制到MCU端以源程序的形式被装在MCU程序里。这么做是为了调试
+ */
+static void print_usage(const char *argv0) {
+    fprintf(stderr, "Usage: %s -r private-key.pem -u public-key.pem -o output.bin -e executable.bin [-i]\n", argv0);
+    fprintf(stderr, "  -i means to generate .inc file for the whole firmware, then let MCU program to test\n");
     exit(1);
 }
 
-size_t load_execable_into_firmware_memory(const char *exec_filename) {
+/*
+    装入“可执行体”，参数名字就是bin的名字。返回值是bin的大小。出错直接退出
+    装入的地方在前面的firmware_memory里
+
+ */
+static size_t load_execable_into_firmware_memory(const char *exec_filename) {
     FILE *f = fopen(exec_filename, "rb");
     if (f == nullptr) {
         fprintf(stderr, "cannot open execable file '%s' to read\n", exec_filename);
         exit(1);
     }
-    auto read_bytes = fread(firmware_memory + EXEC_BLOCK_POS, 1, EXEC_BLOCK_MAX_SIZE, f);
+    auto read_bytes = fread(firmware_memory + EXEC_BODY_POS, 1, EXEC_BODY_MAX_SIZE, f);
     bool is_ok = !ferror(f);
     is_ok &= feof(f);
-    is_ok &= read_bytes < EXEC_BLOCK_MAX_SIZE;
+    is_ok &= read_bytes < EXEC_BODY_MAX_SIZE;
     fclose(f);
     if (!is_ok) {
         fprintf(stderr, "reading execable file '%s' failed\n", exec_filename);
@@ -35,8 +53,12 @@ size_t load_execable_into_firmware_memory(const char *exec_filename) {
     return read_bytes;
 }
 
-void set_exec_len_into_firmware_memory(size_t size) {
-    if (size >= EXEC_BLOCK_MAX_SIZE) {
+/*
+    把“可执行体”的长度按照布局填入firmware_memory中。出错直接退出
+    布局中采用小端存放，可移植的写法
+ */
+static void set_exec_len_into_firmware_memory(size_t size) {
+    if (size >= EXEC_BODY_MAX_SIZE) {
         fprintf(stderr, "executable file size is too big");
         exit(1);
     }
@@ -46,7 +68,9 @@ void set_exec_len_into_firmware_memory(size_t size) {
     }
 }
 
-size_t get_exec_len_inside_firmware_memory() {
+// 从firmware_memory中读出“可执行体”的长度，并返回
+// 逐字节读出并拼出来，可移植的写法
+static size_t get_exec_len_inside_firmware_memory() {
     size_t ret = 0;
     for (size_t i = 0; i < EXEC_LEN_SIZE; i++) {
         ret += firmware_memory[EXEC_LEN_ADDR + i] << (8 * i);
@@ -54,29 +78,38 @@ size_t get_exec_len_inside_firmware_memory() {
     return ret;
 }
 
-bool save_firmware_memory_into_file(const char *output_filename) {
+// 把firmware_memory写出到指定的文件中
+// 正确返回true，反之反是
+static bool save_firmware_memory_into_file(const char *output_filename) {
     FILE *f = fopen(output_filename, "wb");
     if (f == nullptr) {
         fprintf(stderr, "cannot open file '%s' for output", output_filename);
     }
     bool is_ok;
-    const auto should_written = EXEC_BLOCK_POS + get_exec_len_inside_firmware_memory();
-    size_t written = fwrite(firmware_memory, 1, EXEC_BLOCK_POS + get_exec_len_inside_firmware_memory(), f);
-    is_ok = written == should_written;
-    is_ok &= fclose(f) == 0;
+    const auto should_written = EXEC_BODY_POS + get_exec_len_inside_firmware_memory();                      // 读出大小
+    size_t written = fwrite(firmware_memory, 1, EXEC_BODY_POS + get_exec_len_inside_firmware_memory(), f);  // 写文件
+    is_ok = written == should_written;                                                                      // 写全了没？
+    is_ok &= !ferror(f);                                                                                    // 出错了没？
+    is_ok &= fclose(f) == 0;                                                                                // 出错了没？
+
     return is_ok;
 }
 
-bool save_firmware_memory_into_inc_file(const char *inc_output_filename) {
+/*
+    把firmware内容打印到指定的文件中，格式是便于直接被C/C++程序#include到程序里的
+    正确返回true，反之反是
+    这么做是因为最开始的时候固件传输还没做好，这个数据可以直接被MCU端包含，就可以测试这部分了。
+ */
+static bool save_firmware_memory_into_inc_file(const char *inc_output_filename) {
     FILE *f = fopen(inc_output_filename, "wb");
     if (f == nullptr) {
         fprintf(stderr, "cannot open file '%s' for inc output", inc_output_filename);
     }
 
     // 打印签名数据的C数组形式。为的是复制后贴入MCU端看那边解码是否正确
-    const auto last_pos = EXEC_BLOCK_POS + get_exec_len_inside_firmware_memory();
+    const auto last_pos = EXEC_BODY_POS + get_exec_len_inside_firmware_memory();
     for (size_t i = 0; i < last_pos; i++) {
-        fprintf(f, "%s", i % 16 == 0 ? "    " : " ");
+        fprintf(f, "%s", i % 16 == 0 ? "    " : " ");  // 往下都是格式控制
         fprintf(f, "0x%02X,", firmware_memory[i]);
         if (i % 16 == 15) {
             fprintf(f, "\n");
@@ -84,18 +117,19 @@ bool save_firmware_memory_into_inc_file(const char *inc_output_filename) {
     }
     fprintf(f, "\n");
     bool is_ok = !ferror(f);
-    is_ok &= fclose(f) == 0;
+    is_ok &= fclose(f) == 0;  // 各种检查
     return is_ok;
 }
 
-// 目前主程序就是一个示例。对 '1234'这个字串签名并验证。
 int main(int argc, char *argv[]) {
+    // 按照用法读入各种选项，存入变量
     const char *private_key_pem_filename = nullptr;
     const char *public_key_pem_filename = nullptr;
     const char *exec_bin_filename = nullptr;
     const char *output_firmware_filename = nullptr;
     bool save_inc_file = false;
 
+    // 保险起见，这些变量不应该存两次，所以带检查
     auto test_and_assign_opt_string_to_variable = [argv](const char *&variable) {
         if (variable != nullptr) {
             print_usage(argv[0]);
@@ -103,6 +137,7 @@ int main(int argc, char *argv[]) {
         variable = optarg;
     };
 
+    // 标准套路，获得选项变量
     for (;;) {
         int opt = getopt(argc, argv, "r:u:o:e:i");
         if (opt == -1) {
@@ -135,6 +170,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // 是不是都有了？
     if (optind != argc ||
         private_key_pem_filename == nullptr ||
         public_key_pem_filename == nullptr ||
@@ -143,25 +179,31 @@ int main(int argc, char *argv[]) {
         print_usage(argv[0]);
     }
 
+    // 开始，先装入“可执行体”
     auto exec_len = load_execable_into_firmware_memory(exec_bin_filename);
+    // 再存入长度
     set_exec_len_into_firmware_memory(exec_len);
 
+    // 对指定区域做摘要以及签名
     bool ret;
     ret = sign_by_sha512_rsa2048_pkcs1_padding(
         private_key_pem_filename,
-        firmware_memory + BEGIN_POS_OF_PLAIN, EXEC_BLOCK_POS + exec_len - BEGIN_POS_OF_PLAIN,
+        firmware_memory + BEGIN_POS_OF_PLAIN, EXEC_BODY_POS + exec_len - BEGIN_POS_OF_PLAIN,
         firmware_memory + SIGNATURE_POS);  // 签名
     if (!ret) {
         fprintf(stderr, "signing signature failed\n");
         exit(1);
     }
     fprintf(stderr, "signing signature: OK\n");
+
+    // 读出写入的“可执行体”长度
     auto exec_len_inside_firmware_memory = get_exec_len_inside_firmware_memory();
     assert(exec_len_inside_firmware_memory == exec_len);
 
+    // 用公钥验证签名
     ret = verify_sign_by_sha512_rsa2048_pkcs1_padding(
         public_key_pem_filename,
-        firmware_memory + BEGIN_POS_OF_PLAIN, EXEC_BLOCK_POS + exec_len_inside_firmware_memory - BEGIN_POS_OF_PLAIN,
+        firmware_memory + BEGIN_POS_OF_PLAIN, EXEC_BODY_POS + exec_len_inside_firmware_memory - BEGIN_POS_OF_PLAIN,
         firmware_memory + SIGNATURE_POS);  // 验证签名
     if (!ret) {
         fprintf(stderr, "signature verification failed\n");
@@ -169,6 +211,7 @@ int main(int argc, char *argv[]) {
     }
     fprintf(stderr, "signature verification: OK\n");
 
+    // 保存固件
     ret = save_firmware_memory_into_file(output_firmware_filename);
     if (!ret) {
         fprintf(stderr, "saving firmware failed\n");
@@ -176,8 +219,9 @@ int main(int argc, char *argv[]) {
     }
     fprintf(stderr, "saving firmware: OK\n");
 
+    // 可选的保存C格式固件
     if (save_inc_file) {
-        const std::string inc_filename = std::string(output_firmware_filename) + ".inc";
+        const std::string inc_filename = std::string(output_firmware_filename) + ".inc"; // 文件名咱也不传入了，直接加一个 .inc 了事
         ret = save_firmware_memory_into_inc_file(inc_filename.c_str());
         if (!ret) {
             fprintf(stderr, "saving firmware.inc failed\n");
