05a281df1a6814061e2e8365fe8a4e92af15c20f
Guo Qiang <guoqiang@huaqin.com>
2023-12-12 18:53:38 +0800

    访问序列换了个写法，增加可读性
    。

--- src/gen_signature.hpp
+++ src/gen_signature.hpp
@@ -9,6 +9,9 @@
     郭强 2023-12-08 更新
     用PSS填充方案换掉了原先的固定填充方案，后者不再被建议使用
     PSS即Probabilistic signature scheme（概率性签名方案）
+    郭强 2023-12-12 更新
+    兼容了Open SSL 1.0版的API
+    优化可读性
 
  */
 #include <openssl/evp.h>
@@ -29,78 +32,49 @@ auto constexpr RSA_PADDING_MODE = RSA_PKCS1_PSS_PADDING;
     origin_data、origin_size是原始数据。不用担心超长，因为签名前会先做摘要，成为固定长度的数据
     sign_data存放签名本身，长度固定是RSA2048_RESULT_LEN
     返回值：true表示成功，否则表示失败
-    TODO: 申请的内存未释放，需要释放
  */
 static inline bool sign_by_sha512_rsa2048_pkcs1_pss_padding(
     const char *rsa_private_key_pem_filename,
     const uint8_t *origin_data, size_t origin_size,
     uint8_t sign_data[SIGNATURE_LEN]) {
-    // 跟签名有关的上下文
+    //
     memset(sign_data, 0, SIGNATURE_LEN);
-    EVP_PKEY *key = nullptr;
-    EVP_MD_CTX *digest_md_ctx = nullptr;
-    const EVP_MD *md = nullptr;
+
+    // 从文件中读取签名用的私钥。成功返回私钥指针，失败返回nullptr
+    auto get_key_from_file = [](const char *filename) {
+        EVP_PKEY *key = nullptr;
+        FILE *f = fopen(filename, "rb");
+        if (f) {
+            key = PEM_read_PrivateKey(f, NULL, NULL, NULL);
+            fclose(f);  // TODO: it might be failed, should check it
+        }
+        return key;
+    };
+
+    // 跟签名有关的上下文
+    EVP_PKEY *key = get_key_from_file(rsa_private_key_pem_filename);
+    EVP_MD_CTX *digest_md_ctx = EVP_MD_CTX_new();
+    const EVP_MD *md = EVP_sha512();
     EVP_PKEY_CTX *pkey_ctx = nullptr;
-    FILE *f = nullptr;
-    bool is_ok = false;
-    do {
-        f = fopen(rsa_private_key_pem_filename, "rb");  // 打开pem文件供读入私钥
-        if (f == NULL) {
-            break;
-        }
-        key = PEM_read_PrivateKey(f, NULL, NULL, NULL);  // 读入私钥
-        if (key == nullptr) {
-            break;
-        }
-        if (EVP_PKEY_id(key) != EVP_PKEY_RSA || EVP_PKEY_bits(key) != SIGN_BITLEN) {  // 检查签名钥的规格
-            break;
-        }
 
-        // 下面的访问序列是标准的签名序列
-        digest_md_ctx = EVP_MD_CTX_new();
-        md = EVP_sha512();
-        if (digest_md_ctx == nullptr || md == nullptr) {
-            break;
-        }
-        if (!EVP_DigestInit(digest_md_ctx, md)) {
-            break;
-        }
-        if (!EVP_DigestSignInit(digest_md_ctx, &pkey_ctx, md, NULL, key)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PADDING_MODE)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_mgf1_md(pkey_ctx, md)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, RSA_PSS_SALTLEN_MAX)) {
-            break;
-        }
-        if (!EVP_DigestSignUpdate(digest_md_ctx, origin_data, origin_size)) {
-            break;
-        }
-        size_t sig_len = SIGNATURE_LEN;
-        if (!EVP_DigestSignFinal(digest_md_ctx, sign_data, &sig_len)) {
-            break;
-        }
-        if (sig_len != SIGNATURE_LEN) {
-            break;
-        }
-        is_ok = true;
-    } while (false);
+    // 一系列的检查和操作，这个操作序列经测试可用于签名过程，并能和MCU端的程序配合
+    bool is_ok = key != nullptr && digest_md_ctx != nullptr && md != nullptr;
+    is_ok = is_ok && EVP_PKEY_id(key) == EVP_PKEY_RSA && EVP_PKEY_bits(key) == SIGN_BITLEN;
+    is_ok = is_ok && EVP_DigestInit(digest_md_ctx, md);
+    is_ok = is_ok && EVP_DigestSignInit(digest_md_ctx, &pkey_ctx, md, NULL, key);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PADDING_MODE);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_mgf1_md(pkey_ctx, md);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, RSA_PSS_SALTLEN_MAX);
+    is_ok = is_ok && EVP_DigestSignUpdate(digest_md_ctx, origin_data, origin_size);
 
-    if (f) {
-        bool r = fclose(f) == 0;
-        is_ok &= r;
-        f = nullptr;
-    }
+    size_t sig_len = SIGNATURE_LEN;
+    is_ok = is_ok && EVP_DigestSignFinal(digest_md_ctx, sign_data, &sig_len);
+    is_ok = is_ok && sig_len == SIGNATURE_LEN;
 
-    // TODO: 申请资源未释放，目前没搞清楚哪些资源需要释放
-    // 作为一次性签名的子函数，不释放没问题
-    // 猜测是下面俩调用：
-    //   EVP_MD_CTX_free(digest_md_ctx);
-    //   EVP_PKEY_free(key);
+    // 释放上下文和key
+    EVP_MD_CTX_free(digest_md_ctx);
+    EVP_PKEY_free(key);
+    // TODO：不确定md是否需要释放
 
     return is_ok;
 }
@@ -111,75 +85,44 @@ static inline bool sign_by_sha512_rsa2048_pkcs1_pss_padding(
     origin_data、origin_size是原始数据。不用担心超长，因为签名前会先做摘要，成为固定长度的数据
     sign_data签名本身，长度固定是RSA2048_RESULT_LEN
     返回值：true表示成功，否则表示失败
-    TODO: 申请的内存未释放，需要释放
  */
 static inline bool verify_sign_by_sha512_rsa2048_pkcs1_pss_padding(
     const char *rsa_public_key_pem_filename,
     const uint8_t *origin_data, size_t origin_size,
     const uint8_t sign_data[SIGNATURE_LEN]) {
+    //
+    // 从文件中读取验证签名用的共钥。成功返回私钥指针，失败返回nullptr
+    auto get_key_from_file = [](const char *filename) {
+        EVP_PKEY *key = nullptr;
+        FILE *f = fopen(filename, "rb");
+        if (f) {
+            key = PEM_read_PUBKEY(f, NULL, NULL, NULL);
+            fclose(f);  // TODO: it might be failed, should check it
+        }
+        return key;
+    };
+
     // 跟签名验证有关的上下文
-    EVP_PKEY *key = nullptr;
-    EVP_MD_CTX *digest_md_ctx = nullptr;
-    const EVP_MD *md = nullptr;
+    EVP_PKEY *key = get_key_from_file(rsa_public_key_pem_filename);
+    EVP_MD_CTX *digest_md_ctx = EVP_MD_CTX_new();
+    const EVP_MD *md = EVP_sha512();
     EVP_PKEY_CTX *pkey_ctx = nullptr;
-    FILE *f = nullptr;
-    bool is_ok = false;
 
-    do {
-        f = fopen(rsa_public_key_pem_filename, "rb");  // 打开pem文件供读入公钥
-        if (f == NULL) {
-            break;
-        }
-        key = PEM_read_PUBKEY(f, NULL, NULL, NULL);  // 读入公钥
-        if (key == nullptr) {
-            break;
-        }
-        if (EVP_PKEY_id(key) != EVP_PKEY_RSA || EVP_PKEY_bits(key) != SIGN_BITLEN) {  // 检查验签钥的规格
-            break;
-        }
+    // 一系列的检查和操作，这个操作序列经测试可用于签名验证过程，并和MCU端的程序结果一致
+    bool is_ok = key != nullptr && digest_md_ctx != nullptr && md != nullptr;
+    is_ok = is_ok && EVP_PKEY_id(key) == EVP_PKEY_RSA && EVP_PKEY_bits(key) == SIGN_BITLEN;
+    is_ok = is_ok && EVP_DigestInit(digest_md_ctx, md);
+    is_ok = is_ok && EVP_DigestVerifyInit(digest_md_ctx, &pkey_ctx, md, NULL, key);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PADDING_MODE);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_mgf1_md(pkey_ctx, md);
+    is_ok = is_ok && EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, RSA_PSS_SALTLEN_MAX);
+    is_ok = is_ok && EVP_DigestVerifyUpdate(digest_md_ctx, origin_data, origin_size);
+    is_ok = is_ok && EVP_DigestVerifyFinal(digest_md_ctx, sign_data, SIGNATURE_LEN);
 
-        // 下面的访问序列是标准的签名验证序列
-        digest_md_ctx = EVP_MD_CTX_new();
-        md = EVP_sha512();
-        if (digest_md_ctx == nullptr || md == nullptr) {
-            break;
-        }
-        if (!EVP_DigestInit(digest_md_ctx, md)) {
-            break;
-        }
-        if (!EVP_DigestVerifyInit(digest_md_ctx, &pkey_ctx, md, NULL, key)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_padding(pkey_ctx, RSA_PADDING_MODE)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_mgf1_md(pkey_ctx, md)) {
-            break;
-        }
-        if (!EVP_PKEY_CTX_set_rsa_pss_saltlen(pkey_ctx, RSA_PSS_SALTLEN_MAX)) {
-            break;
-        }
-        if (!EVP_DigestVerifyUpdate(digest_md_ctx, origin_data, origin_size)) {
-            break;
-        }
-        if (!EVP_DigestVerifyFinal(digest_md_ctx, sign_data, SIGNATURE_LEN)) {
-            break;
-        }
-        is_ok = true;
-    } while (0);
-
-    if (f) {
-        bool r = fclose(f) == 0;
-        is_ok &= r;
-        f = nullptr;
-    }
-
-    // TODO: 申请资源未释放，目前没搞清楚哪些资源需要释放
-    // 作为一次性签名验证的子函数，不释放没问题
-    // 猜测是下面俩调用：
-    //   EVP_MD_CTX_free(digest_md_ctx);
-    //   EVP_PKEY_free(key);
-
+    // 释放上下文和key
+    EVP_MD_CTX_free(digest_md_ctx);
+    EVP_PKEY_free(key);
+    // TODO：不确定md是否需要释放
     return is_ok;
 }
 
